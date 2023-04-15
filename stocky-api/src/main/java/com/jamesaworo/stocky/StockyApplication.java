e18c995174b12a46217aad0fff2999c20786c631
James Aworo <james.aworo@outlook.com>
2023-04-15 19:37:06 +0100

    update(stocky-api): add springboot project for backend api

---
+++ stocky-api/src/main/java/com/jamesaworo/stocky/StockyApplication.java
@@ -0,0 +1,13 @@
+package com.jamesaworo.stocky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockyApplication.class, args);
+	}
+
+}
