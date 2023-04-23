02b5c4d8341a6a078972e0a8c501e43de0957122
James <jamesjunior511@gmail.com>
2023-04-23 15:55:16 +0100

    feat(stocky-web): add tailwind css support
    update(stocky-web): add tailwind layers directive
    
    refactor(stocky-web): clean app component

--- stocky-web/src/app/app.component.ts
+++ stocky-web/src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'stocky-web';
-}
+export class AppComponent {}
