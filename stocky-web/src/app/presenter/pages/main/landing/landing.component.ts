df3ca96904f897b2be6e6ad223a42e2fdbebe05b
James <jamesjunior511@gmail.com>
2023-04-26 21:24:08 +0100

    update(pages): add landing page components

---
+++ stocky-web/src/app/presenter/pages/main/landing/landing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css']
+})
+export class LandingComponent {
+
+}
