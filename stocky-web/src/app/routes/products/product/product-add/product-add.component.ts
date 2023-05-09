ac377de3fce76c36888a0df3a90b6b29313fafa6
James Aworo <james.aworo@outlook.com>
2023-05-09 21:04:36 +0100

    update(products): add create new product component

--- stocky-web/src/app/routes/products/product/product-add/product-add.component.ts
+++ stocky-web/src/app/routes/products/product/product-add/product-add.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Crumbs } from '../../../../shared/components/breadcrumbs/breadcrumbs.component';
 
 @Component({
-  selector: 'app-product-add',
-  templateUrl: './product-add.component.html',
-  styleUrls: ['./product-add.component.css']
+    selector: 'app-product-add',
+    templateUrl: './product-add.component.html',
+    styleUrls: ['./product-add.component.css'],
 })
 export class ProductAddComponent {
+    public showPageHeader = false;
 
+    public crumbs: Crumbs[] = [
+        { link: '/dashboard', title: 'Dashboard' },
+        { link: '/products/product-list', title: 'Product' },
+        { link: '/products/product-add', title: 'Add Product ' },
+    ];
 }
--- stocky-web/src/app/routes/products/products-routing.module.ts
+++ stocky-web/src/app/routes/products/products-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductCategoryListComponent } from './catogory/category-list/product-category-list.component';
 import { ProductVariantListComponent } from './variant/variant-list/product-variant-list.component';
+import { ProductAddComponent } from './product/product-add/product-add.component';
 
 const routes: Routes = [
     { path: 'product-list', component: ProductListComponent },
+    { path: 'product-add', component: ProductAddComponent },
     { path: 'category-list', component: ProductCategoryListComponent },
     { path: 'variants', component: ProductVariantListComponent },
 ];
