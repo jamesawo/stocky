7fd01092d25c00bd37d40e3d30e8584547eb8807
James Aworo <james.aworo@outlook.com>
2023-05-17 10:34:43 +0100

    fix(app-module): fix imports

--- stocky-web/src/app/app.module.ts
+++ stocky-web/src/app/app.module.ts
@@ -1,57 +1,45 @@
-import { HttpClientModule } from '@angular/common/http';
-import { APP_INITIALIZER, LOCALE_ID, NgModule, Type } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NzMessageModule } from 'ng-zorro-antd/message';
-import { NzNotificationModule } from 'ng-zorro-antd/notification';
-import { Observable } from 'rxjs';
+import {registerLocaleData} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
 
-import { default as ngLang } from '@angular/common/locales/en';
-import { DELON_LOCALE, en_US as delonLang } from '@delon/theme';
-import { zhCN as dateLang } from 'date-fns/locale';
-import {
-    en_US as zorroLang,
-    NZ_DATE_LOCALE,
-    NZ_I18N,
-} from 'ng-zorro-antd/i18n';
+import {default as ngLang} from '@angular/common/locales/en';
+import {APP_INITIALIZER, LOCALE_ID, NgModule, Type} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {IconDefinition} from '@ant-design/icons-angular';
+import {CaretLeftOutline, SettingOutline, StepBackwardOutline} from '@ant-design/icons-angular/icons';
+import {StartupService} from '@core';
+import {DELON_LOCALE, en_US as delonLang} from '@delon/theme';
+import {JsonSchemaModule, SharedModule} from '@shared';
+import {zhCN as dateLang} from 'date-fns/locale';
+import {en_US as zorroLang, NZ_DATE_LOCALE, NZ_I18N} from 'ng-zorro-antd/i18n';
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import {NzMessageModule} from 'ng-zorro-antd/message';
+import {NzNotificationModule} from 'ng-zorro-antd/notification';
+import {Observable} from 'rxjs';
 
-import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { GlobalConfigModule } from './global-config.module';
-import { LayoutModule } from './layout/layout.module';
-import { RoutesModule } from './routes/routes.module';
-import { JsonSchemaModule, SharedModule } from '@shared';
-import { STWidgetModule } from './shared/st-widget/st-widget.module';
-import { registerLocaleData } from '@angular/common';
-// import { I18NService, StartupService } from '@core';
-import { StartupService } from '@core';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { IconDefinition } from '@ant-design/icons-angular';
-import {
-    CaretLeftOutline,
-    SettingOutline,
-    StepBackwardOutline,
-} from '@ant-design/icons-angular/icons';
+import {AppComponent} from './app.component';
+import {CoreModule} from './core/core.module';
+import {GlobalConfigModule} from './global-config.module';
+import {LayoutModule} from './layout/layout.module';
+import {RoutesModule} from './routes/routes.module';
+import {STWidgetModule} from './shared/st-widget/st-widget.module';
 
 const LANG = {
     abbr: 'en',
     ng: ngLang,
     zorro: zorroLang,
     date: dateLang,
-    delon: delonLang,
+    delon: delonLang
 };
 
 registerLocaleData(LANG.ng, LANG.abbr);
 const LANG_PROVIDES = [
-    { provide: LOCALE_ID, useValue: LANG.abbr },
-    { provide: NZ_I18N, useValue: LANG.zorro },
-    { provide: NZ_DATE_LOCALE, useValue: LANG.date },
-    { provide: DELON_LOCALE, useValue: LANG.delon },
+    {provide: LOCALE_ID, useValue: LANG.abbr},
+    {provide: NZ_I18N, useValue: LANG.zorro},
+    {provide: NZ_DATE_LOCALE, useValue: LANG.date},
+    {provide: DELON_LOCALE, useValue: LANG.delon}
 ];
 
-// const I18NSERVICE_PROVIDES: any[] = [
-//     { provide: ALAIN_I18N_TOKEN, useClass: I18NService, multi: false },
-// ];
 const FORM_MODULES = [JsonSchemaModule];
 
 let INTERCEPTOR_PROVIDES: any[] = [
@@ -73,14 +61,14 @@ const APPINIT_PROVIDES = [
         provide: APP_INITIALIZER,
         useFactory: StartupServiceFactory,
         deps: [StartupService],
-        multi: true,
-    },
+        multi: true
+    }
 ];
 
 const icons: IconDefinition[] = [
     StepBackwardOutline,
     CaretLeftOutline,
-    SettingOutline,
+    SettingOutline
 ];
 
 @NgModule({
@@ -99,14 +87,15 @@ const icons: IconDefinition[] = [
         NzNotificationModule,
         NzIconModule.forChild(icons),
         ...FORM_MODULES,
-        ...GLOBAL_THIRD_MODULES,
+        ...GLOBAL_THIRD_MODULES
     ],
     providers: [
         ...LANG_PROVIDES,
         ...INTERCEPTOR_PROVIDES,
         // ...I18NSERVICE_PROVIDES,
-        ...APPINIT_PROVIDES,
+        ...APPINIT_PROVIDES
     ],
-    bootstrap: [AppComponent],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
