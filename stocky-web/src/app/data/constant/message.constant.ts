e72d70e7f4b56e53abdbac66d64a7582f3a743d8
James Aworo <james.aworo@outlook.com>
2023-05-19 14:30:32 +0100

    update(data): add pop over constants

--- stocky-web/src/app/data/constant/message.constant.ts
+++ stocky-web/src/app/data/constant/message.constant.ts
@@ -2,3 +2,10 @@ export class MessageConstant {
     static GENERIC_SUCCESS_MESSAGE = 'Action Successful';
     static CATEGORY_SAVED = 'Category saved!';
 }
+
+export class PopOverConstant {
+    static POP_TITLE = 'HELP';
+    static PRODUCT_CATEGORY_PARENT =
+        'If you are creating a sub category, then ' +
+        'you have to select the parent to which this sub category belongs.';
+}
