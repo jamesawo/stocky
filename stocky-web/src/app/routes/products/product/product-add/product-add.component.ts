9e5e35e76ad3a6a559256a39ce7f05a71cf7dda6
James Aworo <james.aworo@outlook.com>
2023-05-21 12:04:44 +0100

    update(components): refactor product-add component

--- stocky-web/src/app/routes/products/product/product-add/product-add.component.ts
+++ stocky-web/src/app/routes/products/product/product-add/product-add.component.ts
@@ -1,17 +1,41 @@
-import { Component } from '@angular/core';
-import { Crumbs } from '../../../../shared/components/breadcrumbs/breadcrumbs.component';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {PRODUCT_ADD_CRUMBS} from '../../../../data/constant/crumb.constant';
+import {PRODUCT_CREATE_POPOVER} from '../../../../data/constant/message.constant';
 
 @Component({
     selector: 'app-product-add',
     templateUrl: './product-add.component.html',
     styleUrls: ['./product-add.component.css'],
 })
-export class ProductAddComponent {
+export class ProductAddComponent implements OnInit {
+    public startPoint = 0;
+    public tabs?: {title: string; template: TemplateRef<any>}[];
     public showPageHeader = false;
+    public crumbs = PRODUCT_ADD_CRUMBS;
+    public popover = PRODUCT_CREATE_POPOVER;
 
-    public crumbs: Crumbs[] = [
-        { link: '/dashboard', title: 'Dashboard' },
-        { link: '/products/product-list', title: 'Product' },
-        { link: '/products/product-add', title: 'Add Product ' },
-    ];
+    @ViewChild('basicTmpl', {static: true})
+    public basicTmpl!: TemplateRef<any>;
+
+    @ViewChild('priceTmpl', {static: true})
+    public priceTmpl!: TemplateRef<any>;
+
+    @ViewChild('newTmpl', {static: true})
+    public newTmpl!: TemplateRef<any>;
+
+    ngOnInit(): void {
+        this.tabs = [
+            {title: 'BASIC DETAILS', template: this.basicTmpl},
+            {title: 'PRICE & TAX', template: this.priceTmpl},
+            {title: 'VARIATION', template: this.newTmpl},
+        ];
+    }
+
+    public onSaveProduct = () => {
+        console.log('saving product');
+    };
+
+    public onCancelSaveProduct = () => {
+        console.log('cancelled saving product');
+    };
 }
