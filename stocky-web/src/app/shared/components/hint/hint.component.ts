e4c8802c304e4aa72ffe1b2d3efe38a4afece6ee
James Aworo <james.aworo@outlook.com>
2023-05-21 12:06:39 +0100

    update(components): refactor hint tooltip component

--- stocky-web/src/app/shared/components/hint/hint.component.ts
+++ stocky-web/src/app/shared/components/hint/hint.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-hint',
-  templateUrl: './hint.component.html',
-  styles: [
-  ]
+    selector: 'app-hint',
+    templateUrl: './hint.component.html',
+    styles: [],
 })
 export class HintComponent {
+    @Input()
+    public props?: {title?: string; content: any};
 
+    @Input()
+    public content?: string;
 }
