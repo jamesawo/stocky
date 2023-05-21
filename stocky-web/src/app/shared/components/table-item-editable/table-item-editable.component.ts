fd5db3332a7bf832e79c340ad759297deff1bc28
James Aworo <james.aworo@outlook.com>
2023-05-21 12:07:05 +0100

    update(components): add editable table row component

--- stocky-web/src/app/shared/components/table-item-editable/table-item-editable.component.ts
+++ stocky-web/src/app/shared/components/table-item-editable/table-item-editable.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-table-item-editable',
-  templateUrl: './table-item-editable.component.html',
-  styles: [
-  ]
+    selector: 'app-table-item-editable',
+    templateUrl: './table-item-editable.component.html',
+    styles: [],
 })
-export class TableItemEditableComponent {
+export class TableItemEditableComponent<T> {
+    @Input()
+    public class?: string;
 
+    @Input()
+    public props?: {
+        fieldName: string;
+        isEditMode?: boolean;
+        initialValue: any;
+    };
+
+    @Input()
+    public value?: any;
+
+    @Output()
+    public valueChange = new EventEmitter<any>();
+
+    /**
+     * Handles the input event and emits the updated value as the user is typing.
+     * @param {Event} event - The input event object.
+     * @returns {void}
+     */
+    public onInput(event: Event): void {
+        const value: any = (event.target as HTMLInputElement).value;
+        this.valueChange.emit(value);
+    }
 }
