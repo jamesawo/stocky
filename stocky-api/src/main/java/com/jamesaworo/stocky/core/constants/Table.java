c84151531224a14dd04edf2dc3b376f01dd3e916
James Aworo <james.aworo@outlook.com>
2023-05-22 01:24:52 +0100

    update(model): add unit-of-measure model

--- stocky-api/src/main/java/com/jamesaworo/stocky/core/constants/Table.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/core/constants/Table.java
@@ -5,25 +5,27 @@ package com.jamesaworo.stocky.core.constants;
  * @since 4/16/23
  */
 public class Table {
-    public static final String PREFIX = "STOCKY_";
+	public static final String PREFIX = "STOCKY_";
 
-    // SETTING TABLES
-    public static final String SETTING = PREFIX + "SETTING";
-    public static final String SETTING_DASHBOARD = SETTING + "_DASHBOARD";
-    public static final String SETTING_BACKUP_RESTORE = SETTING + "_BACKUP";
-    public static final String SETTING_EXPENSES = SETTING + "_EXPENSES";
-    public static final String SETTING_NOTIFICATION = SETTING + "_NOTIFICATION";
-    public static final String SETTING_PAYMENT_METHOD = SETTING + "_PAYMENT_METHOD";
-    public static final String SETTING_TAX = SETTING + "_TAX";
-    public static final String SETTING_STOCK = SETTING + "_STOCK";
-    public static final String SETTING_PEOPLE = SETTING + "_PEOPLE";
-    public static final String SETTING_PRODUCT = SETTING + "_PRODUCT";
-    public static final String SETTING_SALE = SETTING + "_SALE";
-    public static final String SETTING_OPTIONS = SETTING + "_OPTION";
+	// SETTING TABLES
+	public static final String SETTING = PREFIX + "SETTING";
+	public static final String SETTING_DASHBOARD = SETTING + "_DASHBOARD";
+	public static final String SETTING_BACKUP_RESTORE = SETTING + "_BACKUP";
+	public static final String SETTING_EXPENSES = SETTING + "_EXPENSES";
+	public static final String SETTING_NOTIFICATION = SETTING + "_NOTIFICATION";
+	public static final String SETTING_PAYMENT_METHOD = SETTING + "_PAYMENT_METHOD";
+	public static final String SETTING_TAX = SETTING + "_TAX";
+	public static final String SETTING_STOCK = SETTING + "_STOCK";
+	public static final String SETTING_PEOPLE = SETTING + "_PEOPLE";
+	public static final String SETTING_PRODUCT = SETTING + "_PRODUCT";
+	public static final String SETTING_SALE = SETTING + "_SALE";
+	public static final String SETTING_OPTIONS = SETTING + "_OPTION";
 
-    // PRODUCT TABLES
-    public static final String PRODUCT = PREFIX + "PRODUCT";
-    public static final String PRODUCT_CATEGORY = PRODUCT + "_CATEGORIES";
-    public static final String PRODUCT_VARIATIONS = PRODUCT + "_VARIATIONS";
+	// PRODUCT TABLES
+	public static final String PRODUCT = PREFIX + "PRODUCT";
+	public static final String PRODUCT_CATEGORY = PRODUCT + "_CATEGORIES";
+	public static final String PRODUCT_VARIATIONS = PRODUCT + "_VARIATIONS";
+	public static final String PRODUCT_UNIT_OF_MEASURE = PRODUCT + "_UNIT_OF_MEASURES";
+
 
 }
