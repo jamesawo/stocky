47d19a0c68c3dc93c26c855cc0ef8f9ec54de167
James Aworo <james.aworo@outlook.com>
2023-05-22 20:01:00 +0100

    update(components): add product status button

--- stocky-web/src/app/routes/products/_components/product-status/product-status-add-btn/product-status-add-btn.component.ts
+++ stocky-web/src/app/routes/products/_components/product-status/product-status-add-btn/product-status-add-btn.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-product-status-add-btn',
-  templateUrl: './product-status-add-btn.component.html',
-  styles: [
-  ]
+    selector: 'app-product-status-add-btn',
+    templateUrl: './product-status-add-btn.component.html',
+    styles: [],
 })
 export class ProductStatusAddBtnComponent {
-
+    public isVisible = false;
+    public showModal = () => (this.isVisible = true);
 }
