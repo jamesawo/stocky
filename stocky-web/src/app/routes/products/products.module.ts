f44ea3dc8ad9acec9e886a75b479684c54b29ba9
James Aworo <james.aworo@outlook.com>
2023-05-23 18:24:31 +0100

    update(module): add new components to product module

--- stocky-web/src/app/routes/products/products.module.ts
+++ stocky-web/src/app/routes/products/products.module.ts
@@ -6,6 +6,9 @@ import {ProductPriceTabComponent} from './_components/product-price-tab/product-
 import {ProductStatusAddBtnComponent} from './_components/product-status/product-status-add-btn/product-status-add-btn.component';
 import {ProductStatusAddComponent} from './_components/product-status/product-status-add/product-status-add.component';
 import {ProductStatusDropdownComponent} from './_components/product-status/product-status-dropdown/product-status-dropdown.component';
+import {ProductTaxAddBtnComponent} from './_components/product-tax/product-tax-add-btn/product-tax-add-btn.component';
+import {ProductTaxAddComponent} from './_components/product-tax/product-tax-add/product-tax-add.component';
+import {ProductTaxDropdownComponent} from './_components/product-tax/product-tax-dropdown/product-tax-dropdown.component';
 import {UnitOfMeasurementDropdownComponent} from './_components/unit-of-measurement/unit-of-measure-dropdown/unit-of-measurement-dropdown.component';
 import {UnitOfMeasurementAddComponent} from './_components/unit-of-measurement/unit-of-measurement-add/unit-of-measurement-add.component';
 import {ProductCategoryListComponent} from './catogory/category-list/product-category-list.component';
@@ -28,6 +31,9 @@ export const PRODUCT_COMPONENTS = [
     ProductStatusAddComponent,
     ProductStatusDropdownComponent,
     ProductStatusAddBtnComponent,
+    ProductTaxDropdownComponent,
+    ProductTaxAddBtnComponent,
+    ProductTaxAddComponent,
 ];
 
 @NgModule({
