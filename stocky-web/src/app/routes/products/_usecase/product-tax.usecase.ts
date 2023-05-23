eec55244f0ce807ab9b886a07da3cc252c493656
James Aworo <james.aworo@outlook.com>
2023-05-23 18:21:25 +0100

    update(usecase): add tax setup usecase

--- stocky-web/src/app/routes/products/_usecase/product-tax.usecase.ts
+++ stocky-web/src/app/routes/products/_usecase/product-tax.usecase.ts
@@ -2,35 +2,35 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {environment} from '@env/environment';
 import {BehaviorSubject, Observable} from 'rxjs';
-import {ProductStatusPayload} from '../_data/product.payload';
+import {ProductTaxPayload} from '../_data/product.payload';
 
 @Injectable({
     providedIn: 'root',
 })
-export class ProductStatusUsecase {
+export class ProductTaxUsecase {
     public trigger: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
     public trigger$: Observable<boolean> = this.trigger.asObservable();
 
-    private url = environment.api.baseUrl + '/product-status';
+    private url = environment.api.baseUrl + '/product-tax';
 
     constructor(private http: HttpClient) {}
 
     public get(id: number) {
-        return this.http.get<ProductStatusPayload>(`${this.url}/get/{id}`);
+        return this.http.get<ProductTaxPayload>(`${this.url}/get/{id}`);
     }
 
     public getMany() {
-        return this.http.get<ProductStatusPayload[]>(`${this.url}/all`);
+        return this.http.get<ProductTaxPayload[]>(`${this.url}/all`);
     }
 
-    public save(data: ProductStatusPayload) {
-        return this.http.post<ProductStatusPayload>(`${this.url}/create`, data, {
+    public save(data: ProductTaxPayload) {
+        return this.http.post<ProductTaxPayload>(`${this.url}/create`, data, {
             observe: 'response',
         });
     }
 
-    public update(category: ProductStatusPayload) {
-        return this.http.put<ProductStatusPayload>(`${this.url}/update`, category, {
+    public update(category: ProductTaxPayload) {
+        return this.http.put<ProductTaxPayload>(`${this.url}/update`, category, {
             observe: 'response',
         });
     }
