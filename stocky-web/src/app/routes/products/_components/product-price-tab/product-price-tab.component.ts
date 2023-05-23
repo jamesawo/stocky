c5b12ca80478c296afe851218c16aaf54bb7a4c3
James Aworo <james.aworo@outlook.com>
2023-05-23 18:22:25 +0100

    update(components): update price tab

--- stocky-web/src/app/routes/products/_components/product-price-tab/product-price-tab.component.ts
+++ stocky-web/src/app/routes/products/_components/product-price-tab/product-price-tab.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {PRODUCT_CREATE_POPOVER} from '../../../../data/constant/message.constant';
 
 @Component({
-  selector: 'app-product-price-tab',
-  templateUrl: './product-price-tab.component.html',
-  styles: [
-  ]
+    selector: 'app-product-price-tab',
+    templateUrl: './product-price-tab.component.html',
+    styles: [],
 })
 export class ProductPriceTabComponent {
-
+    public popover = PRODUCT_CREATE_POPOVER;
 }
