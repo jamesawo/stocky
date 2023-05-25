1717672e3dc1a8b992ce4124b05bb0454e483161
James Aworo <james.aworo@outlook.com>
2023-05-25 10:52:00 +0100

    update(components): update product basic tabs

--- stocky-web/src/app/routes/products/_components/product-basic-tab/product-basic-tab.component.ts
+++ stocky-web/src/app/routes/products/_components/product-basic-tab/product-basic-tab.component.ts
@@ -1,8 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {AbstractControl, FormGroup} from '@angular/forms';
 import {Observable, of} from 'rxjs';
 import {PRODUCT_CREATE_POPOVER} from '../../../../data/constant/message.constant';
-import {ProductStatusPayload} from '../../_data/product.payload';
-import {UnitOfMeasurePayload} from '../../_data/unit-of-measure.payload';
+import {ProductUnitOfMeasurePayload} from '../../_data/product-unit-of-measure.payload';
+import {ProductCategoryPayload, ProductStatusPayload} from '../../_data/product.payload';
+import {FormProps} from '../../_data/product.types';
 
 @Component({
     selector: 'app-product-basic-tab',
@@ -10,14 +12,24 @@ import {UnitOfMeasurePayload} from '../../_data/unit-of-measure.payload';
     styles: [],
 })
 export class ProductBasicTabComponent {
+
+    @Input()
+    formGroup?: FormGroup;
+
     public popover = PRODUCT_CREATE_POPOVER;
     public statusList: Observable<ProductStatusPayload[]> = of([]);
 
-    public onUnitOfMeasureSelected(payload?: UnitOfMeasurePayload) {
+
+    public onUnitOfMeasureSelected(payload?: ProductUnitOfMeasurePayload) {
         console.log(payload);
     }
 
     public onProductStatusSelected(payload: ProductStatusPayload) {
         console.log(payload);
     }
+
+    public onProductCategorySelected(category: ProductCategoryPayload) {
+        console.log(category);
+    }
+
 }
