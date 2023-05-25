bad3c473eb12e061d2866f41f3946509f4214841
James Aworo <james.aworo@outlook.com>
2023-05-25 10:58:13 +0100

    update(components): refactor product add component

--- stocky-web/src/app/routes/products/product/product-add/product-add.component.ts
+++ stocky-web/src/app/routes/products/product/product-add/product-add.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {PRODUCT_ADD_CRUMBS} from '../../../../data/constant/crumb.constant';
 import {PRODUCT_CREATE_POPOVER} from '../../../../data/constant/message.constant';
+import {ProductPayload} from '../../_data/product.payload';
 
 @Component({
     selector: 'app-product-add',
@@ -13,6 +15,22 @@ export class ProductAddComponent implements OnInit {
     public showPageHeader = false;
     public crumbs = PRODUCT_ADD_CRUMBS;
     public popover = PRODUCT_CREATE_POPOVER;
+    public form: FormGroup = this.fb.group({
+        id: [null],
+        productCategory: [null, [Validators.required]],
+        unitOfMeasure: [null, [Validators.required]],
+        status: [null, [Validators.required]],
+        isActive: [true, [Validators.required]],
+        useQuantity: [false, [Validators.required]],
+        isService: [false, [Validators.required]],
+        minAgeLimit: [13],
+        productName: [null, [Validators.required]],
+        brandName: [null],
+        sku: [null],
+        barcode: [null],
+        description: [null],
+        price: [null],
+    });
 
     @ViewChild('basicTmpl', {static: true})
     public basicTmpl!: TemplateRef<any>;
@@ -23,16 +41,18 @@ export class ProductAddComponent implements OnInit {
     @ViewChild('newTmpl', {static: true})
     public newTmpl!: TemplateRef<any>;
 
-    ngOnInit(): void {
+    constructor(private fb: FormBuilder) {}
+
+    public ngOnInit(): void {
         this.tabs = [
             {title: 'BASIC DETAILS', template: this.basicTmpl},
             {title: 'PRICE & TAX', template: this.priceTmpl},
-            {title: 'VARIATION', template: this.newTmpl},
         ];
     }
 
     public onSaveProduct = () => {
         console.log('saving product');
+        console.log(this.form.value);
     };
 
     public onCancelSaveProduct = () => {
