3de4bf6b5e5752af0848222a868d120c906cfc22
James Aworo <james.aworo@outlook.com>
2023-05-28 22:49:33 +0100

    update(components): add input in product price tab

--- stocky-web/src/app/routes/products/_components/product-price-tab/product-price-tab.component.ts
+++ stocky-web/src/app/routes/products/_components/product-price-tab/product-price-tab.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {FormGroup} from '@angular/forms';
 import {PRODUCT_CREATE_POPOVER} from '../../../../data/constant/message.constant';
 
 @Component({
@@ -8,4 +9,14 @@ import {PRODUCT_CREATE_POPOVER} from '../../../../data/constant/message.constant
 })
 export class ProductPriceTabComponent {
     public popover = PRODUCT_CREATE_POPOVER;
+
+    @Input()
+    public formGroup?: FormGroup;
+
+    public getForm() {
+        return this.formGroup!.get('price') as FormGroup;
+    }
+
+
+
 }
