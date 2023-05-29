9d169c89ecf8af8c98e735f570715fdebf61915f
James Aworo <james.aworo@outlook.com>
2023-05-30 00:22:46 +0100

    update(interactor): add product basic and price interactor

--- stocky-api/src/main/java/com/jamesaworo/stocky/features/product/data/interactor/contract/IProductInteractor.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/product/data/interactor/contract/IProductInteractor.java
@@ -1,8 +1,12 @@
 package com.jamesaworo.stocky.features.product.data.interactor.contract;
 
+import com.jamesaworo.stocky.features.product.data.request.ProductRequest;
+import org.springframework.http.ResponseEntity;
+
 /**
  * @author Aworo James
  * @since 5/10/23
  */
 public interface IProductInteractor {
+	ResponseEntity<ProductRequest> save(ProductRequest request);
 }
--- stocky-api/src/main/java/com/jamesaworo/stocky/features/product/data/interactor/implementation/ProductInteractor.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/product/data/interactor/implementation/ProductInteractor.java
@@ -1,12 +1,53 @@
 package com.jamesaworo.stocky.features.product.data.interactor.implementation;
 
 import com.jamesaworo.stocky.core.annotations.Interactor;
+import com.jamesaworo.stocky.core.mapper.Mapper;
 import com.jamesaworo.stocky.features.product.data.interactor.contract.IProductInteractor;
+import com.jamesaworo.stocky.features.product.data.request.ProductRequest;
+import com.jamesaworo.stocky.features.product.domain.entity.Product;
+import com.jamesaworo.stocky.features.product.domain.entity.ProductBasic;
+import com.jamesaworo.stocky.features.product.domain.entity.ProductPrice;
+import com.jamesaworo.stocky.features.product.domain.usecase.IProductBasicUsecase;
+import com.jamesaworo.stocky.features.product.domain.usecase.IProductPriceUsecase;
+import com.jamesaworo.stocky.features.product.domain.usecase.IProductUsecase;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 /**
  * @author Aworo James
  * @since 5/10/23
  */
 @Interactor
-public class ProductInteractor implements IProductInteractor {
+@RequiredArgsConstructor
+public class ProductInteractor implements IProductInteractor, Mapper<ProductRequest, Product> {
+
+	private final ModelMapper mapper;
+	private final IProductUsecase usecase;
+	private final IProductBasicUsecase basicUsecase;
+	private final IProductPriceUsecase priceUsecase;
+
+	public ResponseEntity<ProductRequest> save(ProductRequest request) {
+		Product model = toModel(request);
+		Product product = updatePriceAndBasic(model, model.getPrice(), model.getBasic());
+		return ok(this.toRequest(this.usecase.save(product)));
+	}
+
+	private Product updatePriceAndBasic(Product model, ProductPrice price, ProductBasic basic) {
+		model.setBasic(this.basicUsecase.save(basic));
+		model.setPrice(this.priceUsecase.save(price));
+		return model;
+	}
+
+	public ProductRequest toRequest(Product model) {
+		return this.mapper.map(model, ProductRequest.class);
+	}
+
+	public Product toModel(ProductRequest request) {
+		return this.mapper.map(request, Product.class);
+	}
+
+
 }
