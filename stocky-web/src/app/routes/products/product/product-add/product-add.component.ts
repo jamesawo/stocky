90f01d7ca20e1552f34001c329556cf37d001fe8
James Aworo <james.aworo@outlook.com>
2023-05-29 14:15:33 +0100

    update(components): update add product component, refactor onSave method

--- stocky-web/src/app/routes/products/product/product-add/product-add.component.ts
+++ stocky-web/src/app/routes/products/product/product-add/product-add.component.ts
@@ -3,12 +3,12 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {NzNotificationService} from 'ng-zorro-antd/notification';
 import {PRODUCT_ADD_CRUMBS} from '../../../../data/constant/crumb.constant';
 import {Message, PRODUCT_CREATE_POPOVER} from '../../../../data/constant/message.constant';
-import {isFormInvalid} from '../../../../shared/utils/util';
+import {markFormFieldsAsDirtyAndTouched} from '../../../../shared/utils/util';
 
 @Component({
     selector: 'app-product-add',
     templateUrl: './product-add.component.html',
-    styleUrls: ['./product-add.component.css'],
+    styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent implements OnInit {
     public startPoint = 0;
@@ -18,24 +18,26 @@ export class ProductAddComponent implements OnInit {
     public popover = PRODUCT_CREATE_POPOVER;
     public form: FormGroup = this.fb.group({
         id: [null],
-        productCategory: [null, [Validators.required]],
-        unitOfMeasure: [null, [Validators.required]],
-        status: [null, [Validators.required]],
-        isActive: [true, [Validators.required]],
-        useQuantity: [false, [Validators.required]],
-        isService: [false, [Validators.required]],
-        minAgeLimit: [13],
-        productName: [null, [Validators.required]],
-        brandName: [null],
-        sku: [null],
-        barcode: [null],
-        description: [null],
+        basic: this.fb.group({
+            productCategory: [null, [Validators.required]],
+            unitOfMeasure: [null, [Validators.required]],
+            status: [null, [Validators.required]],
+            isActive: [true, [Validators.required]],
+            useQuantity: [true, [Validators.required]],
+            isService: [false, [Validators.required]],
+            minAgeLimit: [13],
+            productName: [null, [Validators.required]],
+            brandName: [null, [Validators.required]],
+            sku: [null],
+            barcode: [null],
+            description: [null]
+        }),
         price: this.fb.group({
             markup: [null],
             taxes: [[], null],
             costPrice: [null],
-            sellingPrice: [null],
-        }),
+            sellingPrice: [null]
+        })
     });
 
     @ViewChild('basicTmpl', {static: true})
@@ -49,17 +51,20 @@ export class ProductAddComponent implements OnInit {
     public ngOnInit(): void {
         this.tabs = [
             {title: 'BASIC DETAILS', template: this.basicTmpl},
-            {title: 'PRICE & TAX', template: this.priceTmpl},
+            {title: 'PRICE & TAX', template: this.priceTmpl}
         ];
     }
 
     public onSaveProduct = (): void => {
-        const invalid = isFormInvalid(this.form);
-        console.log(invalid);
-        if (invalid) {
-            this.notification.warning(Message.INVALID_FORM_FIELDS, Message.INVALID_ENTRY_TRY_AGAIN);
+        if (this.form.invalid) {
+            markFormFieldsAsDirtyAndTouched(this.form);
+            this.notification.create(
+                'warning', Message.VALIDATION_ERROR,
+                Message.INVALID_FORM_FIELDS, {nzPlacement: 'top'}
+            );
             return;
         }
+
         console.log(this.form.value);
     };
 
