7ff570ca4c1aef7d7a459187e78579a041224d4f
James Aworo <james.aworo@outlook.com>
2023-05-29 14:11:02 +0100

    update(components): update product basic tab form-controls

--- stocky-web/src/app/routes/products/_components/product-basic-tab/product-basic-tab.component.ts
+++ stocky-web/src/app/routes/products/_components/product-basic-tab/product-basic-tab.component.ts
@@ -1,28 +1,33 @@
 import {Component, Input} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {PRODUCT_CREATE_POPOVER} from '../../../../data/constant/message.constant';
-import {isFormControlInvalid} from '../../../../shared/utils/util';
+import {getFormControlValidityStatus, getFormGroupFromParent} from '../../../../shared/utils/util';
 
 @Component({
     selector: 'app-product-basic-tab',
     templateUrl: './product-basic-tab.component.html',
     styles: [
         `
-            ::ng-deep .ng-tns-c18-16.ant-form-item-explain-error {
-                margin-bottom: 5px;
+            :host ::ng-deep div[role="alert"] {
+                margin-bottom: 25px;
             }
+
         `
-    ],
+    ]
 })
 export class ProductBasicTabComponent {
     public popover = PRODUCT_CREATE_POPOVER;
 
     @Input()
     public formGroup?: FormGroup;
-    protected readonly isFormControlInvalid = isFormControlInvalid;
 
-    public isControlValid(controlName: string): any {
-        const control = this.formGroup!.get(controlName);
-        return control && control.invalid && control.dirty ? 'error' : 'success';
+    valid(name: string) {
+        return getFormControlValidityStatus(this.getForm(), name);
     }
+
+    public getForm() {
+        return getFormGroupFromParent(this.formGroup!, 'basic');
+    }
+
+
 }
