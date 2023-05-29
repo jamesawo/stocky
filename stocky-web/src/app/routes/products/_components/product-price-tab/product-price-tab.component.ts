cf21323cb5d94a8f437f43399d000275b5005693
James Aworo <james.aworo@outlook.com>
2023-05-29 14:12:30 +0100

    update(price-tab): add calculate selling price from cost price and markup

--- stocky-web/src/app/routes/products/_components/product-price-tab/product-price-tab.component.ts
+++ stocky-web/src/app/routes/products/_components/product-price-tab/product-price-tab.component.ts
@@ -1,22 +1,34 @@
 import {Component, Input} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {PRODUCT_CREATE_POPOVER} from '../../../../data/constant/message.constant';
+import {getFormGroupFromParent} from '../../../../shared/utils/util';
 
 @Component({
     selector: 'app-product-price-tab',
     templateUrl: './product-price-tab.component.html',
-    styles: [],
+    styles: []
 })
 export class ProductPriceTabComponent {
     public popover = PRODUCT_CREATE_POPOVER;
 
     @Input()
     public formGroup?: FormGroup;
+    protected readonly getSubFormGroup = getFormGroupFromParent;
 
-    public getForm() {
-        return this.formGroup!.get('price') as FormGroup;
+    public getForm(): FormGroup<any> {
+        return getFormGroupFromParent(this.formGroup!, 'price');
     }
 
+    public calculateSellingPrice() {
+        if (this.formGroup) {
+            const form = this.getForm();
+            const costPrice = form.get('costPrice')?.value;
+            const markupPercent = form.get('markup')?.value;
 
+            const markupAmount = Number(costPrice) * Number((markupPercent / 100));
+            const sellingPrice = Number(costPrice) + Number(markupAmount);
 
+            form.get('sellingPrice')?.setValue(sellingPrice);
+        }
+    }
 }
