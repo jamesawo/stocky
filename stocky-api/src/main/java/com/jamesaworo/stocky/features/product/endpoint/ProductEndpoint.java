9709ff3fa8f5b4b09bf3d569825cf3a2d19b333d
James Aworo <james.aworo@outlook.com>
2023-05-30 00:26:46 +0100

    update(domain): add create product basic and price endpoint

--- stocky-api/src/main/java/com/jamesaworo/stocky/features/product/endpoint/ProductEndpoint.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/product/endpoint/ProductEndpoint.java
@@ -1,7 +1,11 @@
 package com.jamesaworo.stocky.features.product.endpoint;
 
 import com.jamesaworo.stocky.features.product.data.interactor.contract.IProductInteractor;
+import com.jamesaworo.stocky.features.product.data.request.ProductRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +19,11 @@ import static com.jamesaworo.stocky.core.constants.Global.API_PREFIX;
 @RequestMapping(value = API_PREFIX + "/product")
 @RequiredArgsConstructor
 public class ProductEndpoint {
-    private final IProductInteractor interactor;
+
+	private final IProductInteractor interactor;
+
+	@PostMapping(value = "/create")
+	public ResponseEntity<ProductRequest> create(@RequestBody ProductRequest request) {
+		return this.interactor.save(request);
+	}
 }
