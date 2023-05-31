3db88793888f3580fe841059a07344a34067916f
James Aworo <james.aworo@outlook.com>
2023-05-31 22:04:01 +0100

    update(data): add product list to crumb constant

--- stocky-web/src/app/data/constant/crumb.constant.ts
+++ stocky-web/src/app/data/constant/crumb.constant.ts
@@ -3,11 +3,17 @@ import {Crumbs} from '../../shared/components/breadcrumbs/breadcrumbs.component'
 export const PRODUCT_CATEGORY_LIST_CRUMBS: Crumbs[] = [
     {link: '/dashboard', title: 'Dashboard'},
     {link: '/products/product-list', title: 'Product'},
-    {link: '/products/category-list', title: 'Categories'},
+    {link: '/products/category-list', title: 'Categories'}
 ];
 
 export const PRODUCT_ADD_CRUMBS: Crumbs[] = [
     {link: '/dashboard', title: 'Dashboard'},
     {link: '/products/product-list', title: 'Product'},
-    {link: '/products/product-add', title: 'Add Product '},
+    {link: '/products/product-add', title: 'Add Product '}
+];
+
+export const PRODUCT_LIST_CRUMBS: Crumbs[] = [
+    {link: '/dashboard', title: 'Dashboard'},
+    {link: '/products/product-list', title: 'Product'},
+    {link: '/products/product-list', title: 'All Products '}
 ];
