844a44ba355dec26db3854e5a5c63b7357fcb17f
James Aworo <james.aworo@outlook.com>
2023-06-01 22:19:20 +0100

    update(components): update date-range-picker

--- stocky-web/src/app/shared/components/range-date-picker/range-date-picker.component.ts
+++ stocky-web/src/app/shared/components/range-date-picker/range-date-picker.component.ts
@@ -1,19 +1,18 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { format, parse } from 'date-fns';
-import { IDateRange } from '../../types/shared.interface';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {format, parse} from 'date-fns';
+import {IDateRange} from 'src/app/data/payload/common.interface';
 
 @Component({
-  selector: 'app-range-date-picker',
-  templateUrl: './range-date-picker.component.html',
-  styles: [
-  ]
+    selector: 'app-range-date-picker',
+    templateUrl: './range-date-picker.component.html',
+    styles: []
 })
 export class RangeDatePickerComponent implements OnInit {
 
     public selectedDate: Date[] | undefined;
     public dateFormat: string = 'yyyy-MM-dd';
 
-    @Input('default') // did not use two way binding on purpose
+    @Input('default') // did not use two-way binding on purpose
     public defaultSelected: string[] = [];
 
     @Output('selected')
@@ -28,23 +27,23 @@ export class RangeDatePickerComponent implements OnInit {
         }
     }
 
-    public setDefaultDate(value: string[]): void{
-         const startDate = parse(value[0], this.dateFormat, new Date());
-         const endDate = parse(value[1], this.dateFormat, new Date());
-         this.selectedDate?.push(...[startDate, endDate])
+    public setDefaultDate(value: string[]): void {
+        const startDate = parse(value[0], this.dateFormat, new Date());
+        const endDate = parse(value[1], this.dateFormat, new Date());
+        this.selectedDate?.push(...[startDate, endDate]);
     }
 
     public onChange(dateValue: Date[]): void {
         if (dateValue.length) {
             let date1 = format(dateValue[0], this.dateFormat);
             let date2 = format(dateValue[1], this.dateFormat);
-            this.selected.emit({ startDate: date1, endDate: date2 });
-        }else{
+            this.selected.emit({startDate: date1, endDate: date2});
+        } else {
             this.selected.emit(undefined);
         }
     }
 
-    public onClear(){
+    public onClear() {
         this.selectedDate = [];
     }
 
