77981c6002469afb7f6880ef44cbf5f2c1d6288c
James Aworo <james.aworo@outlook.com>
2023-06-01 13:31:04 +0100

    update(model): add EntityListeners to base model

--- stocky-api/src/main/java/com/jamesaworo/stocky/core/base/BaseModel.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/core/base/BaseModel.java
@@ -7,14 +7,21 @@
 
 package com.jamesaworo.stocky.core.base;
 
+import lombok.Data;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@MappedSuperclass
 public class BaseModel {
 	@CreatedDate
 	@Column(nullable = false, updatable = false)
