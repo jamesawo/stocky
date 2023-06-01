e5a29c9c4b64d81b9ddb44c8d7393abaf6236542
James Aworo <james.aworo@outlook.com>
2023-06-01 22:13:27 +0100

    update(components): add non-editable table cell

--- stocky-web/src/app/shared/components/table-item/table-item.component.ts
+++ stocky-web/src/app/shared/components/table-item/table-item.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-table-item',
-  templateUrl: './table-item.component.html',
-  styles: [
-  ]
+    selector: 'app-table-item',
+    templateUrl: './table-item.component.html',
+    styles: []
 })
 export class TableItemComponent {
+
+    @Input()
+    content?: string;
+
+    @Input()
+    public props: {useProjection: boolean} = {useProjection: false};
 
 }
