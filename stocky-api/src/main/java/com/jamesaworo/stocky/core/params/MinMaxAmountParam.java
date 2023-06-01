182d3394f9bf708d4aa2a090f248b44a38f538c7
James Aworo <james.aworo@outlook.com>
2023-06-01 13:36:07 +0100

    update(core): add daterange and min-max-amount common params

---
+++ stocky-api/src/main/java/com/jamesaworo/stocky/core/params/MinMaxAmountParam.java
@@ -0,0 +1,16 @@
+/*
+ * @Author: james.junior
+ * @Date: 6/1/23 11:14
+ *
+ * @Project: stocky-api
+ */
+
+package com.jamesaworo.stocky.core.params;
+
+import lombok.Data;
+
+@Data
+public class MinMaxAmountParam {
+	private Double minAmount;
+	private Double maxAmount;
+}
