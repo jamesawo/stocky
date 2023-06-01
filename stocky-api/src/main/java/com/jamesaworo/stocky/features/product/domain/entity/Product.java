29aee7e1b14b34848b65e040a885f123e04bf690
James Aworo <james.aworo@outlook.com>
2023-06-01 22:18:22 +0100

    update(product): add bidirectional mapping in product and product-basic models

--- stocky-api/src/main/java/com/jamesaworo/stocky/features/product/domain/entity/Product.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/product/domain/entity/Product.java
@@ -1,9 +1,7 @@
 package com.jamesaworo.stocky.features.product.domain.entity;
 
 import com.jamesaworo.stocky.core.base.BaseModel;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -18,6 +16,9 @@ import static com.jamesaworo.stocky.core.constants.Table.PRODUCT;
 @Table(name = PRODUCT)
 @Getter
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product extends BaseModel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,6 +29,6 @@ public class Product extends BaseModel {
 
 	@OneToOne
 	private ProductPrice price;
-	
+
 
 }
--- stocky-api/src/main/java/com/jamesaworo/stocky/features/product/domain/entity/ProductBasic.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/product/domain/entity/ProductBasic.java
@@ -33,6 +33,7 @@ public class ProductBasic extends BaseModel {
 
 	@ManyToOne
 	private ProductUnitOfMeasure unitOfMeasure;
+	
 	@ManyToOne
 	private ProductStatus status;
 
