cf126f8f52b43bf74c28f304bab29c3a3690a67f
James Aworo <james.aworo@outlook.com>
2023-06-02 19:25:12 +0100

    update(stock): update stock module

--- stocky-web/src/app/routes/stock/stock.module.ts
+++ stocky-web/src/app/routes/stock/stock.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
-import { StockRoutingModule } from './stock-routing.module';
+import {NgModule, Type} from '@angular/core';
+import {SharedModule} from '@shared';
+import {ManageStockComponent} from './manage-stock/manage-stock.component';
+import {ReconcileStockComponent} from './reconcile-stock/reconcile-stock.component';
+import {RecordDamagedStockComponent} from './record-damaged-stock/record-damaged-stock.component';
+import {RecordStockBalanceComponent} from './record-stock-balance/record-stock-balance.component';
+import {StockRoutingModule} from './stock-routing.module';
+import {ViewLowStockComponent} from './view-low-stock/view-low-stock.component';
+import {ViewStockCountComponent} from './view-stock-count/view-stock-count.component';
+
+export const STOCK_COMPONENTS: Array<Type<void>> = [
+    ManageStockComponent,
+    ViewStockCountComponent,
+    ViewLowStockComponent,
+    ReconcileStockComponent,
+    RecordStockBalanceComponent,
+    RecordDamagedStockComponent
+];
 
 @NgModule({
-    imports: [StockRoutingModule],
+    imports: [StockRoutingModule, SharedModule],
+    declarations: [...STOCK_COMPONENTS
+    ],
+    exports: [...STOCK_COMPONENTS]
 })
-export class StockModule {}
+export class StockModule {
+}
