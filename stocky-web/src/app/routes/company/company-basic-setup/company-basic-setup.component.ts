d50cfa68d00f9cba38673485148a6213d3b5653f
James Aworo <james.aworo@outlook.com>
2023-06-03 21:51:19 +0100

    update(components): update basic tax component, add tabs to group section

--- stocky-web/src/app/routes/company/company-basic-setup/company-basic-setup.component.ts
+++ stocky-web/src/app/routes/company/company-basic-setup/company-basic-setup.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+
+export type PanelData = {
+    active: boolean,
+    name: string,
+    disabled: boolean,
+    tmpl: TemplateRef<any>
+}
 
 @Component({
-  selector: 'app-company-basic-setup',
-  templateUrl: './company-basic-setup.component.html',
-  styles: [
-  ]
+    selector: 'app-company-basic-setup',
+    templateUrl: './company-basic-setup.component.html',
+    styles: []
 })
-export class CompanyBasicSetupComponent {
+export class CompanyBasicSetupComponent implements OnInit {
+    public panels: PanelData[] = [];
+    public data: any;
+
+    @ViewChild('basicTmpl', {static: true})
+    public basicTmpl!: TemplateRef<any>;
+
+    @ViewChild('administratorTmpl', {static: true})
+    public administratorTmpl!: TemplateRef<any>;
+
+    @ViewChild('regionTmpl', {static: true})
+    public regionTmpl!: TemplateRef<any>;
+
+    public ngOnInit(): void {
+        this.panels = [
+            {
+                active: true,
+                name: 'BASIC DETAILS',
+                disabled: false,
+                tmpl: this.basicTmpl
+            },
+            {
+                active: false,
+                disabled: false,
+                name: 'ADMINISTRATOR DETAILS',
+                tmpl: this.administratorTmpl
+            },
+            {
+                active: false,
+                disabled: false,
+                name: 'REGION AND LOCALE DETAILS',
+                tmpl: this.regionTmpl
+            }
+        ];
+    }
 
 }
