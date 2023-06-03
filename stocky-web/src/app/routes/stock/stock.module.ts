1eeb6008c1d1adabac06bfdf89ae1f9446ae1fff
James Aworo <james.aworo@outlook.com>
2023-06-03 01:44:12 +0100

    update(module): update stock module

--- stocky-web/src/app/routes/stock/stock.module.ts
+++ stocky-web/src/app/routes/stock/stock.module.ts
@@ -1,5 +1,6 @@
 import {NgModule, Type} from '@angular/core';
 import {SharedModule} from '@shared';
+import {ProductsModule} from '../products/products.module';
 import {ManageStockComponent} from './manage-stock/manage-stock.component';
 import {ReconcileStockComponent} from './reconcile-stock/reconcile-stock.component';
 import {RecordDamagedStockComponent} from './record-damaged-stock/record-damaged-stock.component';
@@ -18,7 +19,7 @@ export const STOCK_COMPONENTS: Array<Type<void>> = [
 ];
 
 @NgModule({
-    imports: [StockRoutingModule, SharedModule],
+    imports: [StockRoutingModule, SharedModule, ProductsModule],
     declarations: [...STOCK_COMPONENTS
     ],
     exports: [...STOCK_COMPONENTS]
