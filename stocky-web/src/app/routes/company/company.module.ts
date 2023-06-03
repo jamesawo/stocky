9810fdfc1e1e90e9dd8df76b1a3248133e9d92eb
James Aworo <james.aworo@outlook.com>
2023-06-03 14:24:19 +0100

    update(module): update company module, add component declarations

--- stocky-web/src/app/routes/company/company.module.ts
+++ stocky-web/src/app/routes/company/company.module.ts
@@ -2,11 +2,27 @@ import {NgModule, Type} from '@angular/core';
 import {SharedModule} from '@shared';
 import {CompanyLocationSearchComponent} from './_components/company-location-search/company-location-search.component';
 import {CompanyUserSearchComponent} from './_components/company-user-search/company-user-search.component';
+import {CompanyBasicSetupComponent} from './company-basic-setup/company-basic-setup.component';
+import {CompanyExpensesSetupComponent} from './company-expenses-setup/company-expenses-setup.component';
+import {CompanyLocationSetupComponent} from './company-location-setup/company-location-setup.component';
+import {CompanyPaymentOptionsComponent} from './company-payment-options/company-payment-options.component';
+import {CompanyPeopleCustomerComponent} from './company-people/company-people-customer/company-people-customer.component';
+import {CompanyPeopleEmployeesComponent} from './company-people/company-people-employees/company-people-employees.component';
+import {CompanyPeopleSupplierComponent} from './company-people/company-people-supplier/company-people-supplier.component';
 import {CompanyRoutingModule} from './company-routing.module';
+import {CompanyTaxSetupComponent} from './company-tax-setup/company-tax-setup.component';
 
 export const COMPANY_COMPONENTS: Array<Type<void>> = [
     CompanyLocationSearchComponent,
-    CompanyUserSearchComponent
+    CompanyUserSearchComponent,
+    CompanyBasicSetupComponent,
+    CompanyTaxSetupComponent,
+    CompanyPaymentOptionsComponent,
+    CompanyPeopleCustomerComponent,
+    CompanyPeopleEmployeesComponent,
+    CompanyPeopleSupplierComponent,
+    CompanyLocationSetupComponent,
+    CompanyExpensesSetupComponent
 ];
 
 @NgModule({
