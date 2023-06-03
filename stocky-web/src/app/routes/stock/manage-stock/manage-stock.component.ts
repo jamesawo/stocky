54b573c78981879b5fb7b8057769c27ee9ff09d8
James Aworo <james.aworo@outlook.com>
2023-06-03 01:43:44 +0100

    update(components): update manage stock componentts

--- stocky-web/src/app/routes/stock/manage-stock/manage-stock.component.ts
+++ stocky-web/src/app/routes/stock/manage-stock/manage-stock.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {STOCK_MANAGE_CRUMBS} from '../../../data/constant/crumb.constant';
+import {TableCol} from '../../../shared/components/table/table.component';
 
 @Component({
-  selector: 'app-manage-stock',
-  templateUrl: './manage-stock.component.html',
-  styles: [
-  ]
+    selector: 'app-manage-stock',
+    templateUrl: './manage-stock.component.html',
+    styles: []
 })
 export class ManageStockComponent {
+
+    public crumbs = STOCK_MANAGE_CRUMBS;
+    public isOpenHeader = true;
+    public isLoading = false;
+
+    public tableCols: TableCol[] = [
+        {title: 'Stock #'},
+        {title: 'Recorded By'},
+        {title: 'Recorded Date'},
+        {title: 'Recorded items'},
+        {title: 'Total Product Qty'},
+        {title: 'Details'},
+        {title: 'Action'}
+    ];
+
+    public onSearchStock = async (): Promise<void> => {};
+
+    public onResetSearchForm = (): void => {};
+
+    public onCancelHandler = (): void => {};
+
+    public handleCreateStock = (): void => {};
+
 
 }
