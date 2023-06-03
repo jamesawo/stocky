37f645a40d8a4eb38496f2093ba7f945ac99a97e
James Aworo <james.aworo@outlook.com>
2023-06-03 12:52:01 +0100

    update(module): update company module

--- stocky-web/src/app/routes/company/company.module.ts
+++ stocky-web/src/app/routes/company/company.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CompanyRoutingModule } from './company-routing.module';
+import {NgModule, Type} from '@angular/core';
+import {SharedModule} from '@shared';
+import {CompanyLocationSearchComponent} from './_components/company-location-search/company-location-search.component';
+import {CompanyUserSearchComponent} from './_components/company-user-search/company-user-search.component';
+import {CompanyRoutingModule} from './company-routing.module';
+
+export const COMPANY_COMPONENTS: Array<Type<void>> = [
+    CompanyLocationSearchComponent,
+    CompanyUserSearchComponent
+];
 
 @NgModule({
-    imports: [CompanyRoutingModule],
+    imports: [CompanyRoutingModule, SharedModule],
+    declarations: [
+        ...COMPANY_COMPONENTS
+    ],
+    exports: [
+        ...COMPANY_COMPONENTS
+    ]
 })
-export class CompanyModule {}
+export class CompanyModule {
+}
