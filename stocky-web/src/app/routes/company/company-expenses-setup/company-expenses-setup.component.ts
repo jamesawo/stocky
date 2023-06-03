9774dd9fb374178f70430ed888b6cdf5af760160
James Aworo <james.aworo@outlook.com>
2023-06-03 14:22:50 +0100

    update(components): add company basic setup components

---
+++ stocky-web/src/app/routes/company/company-expenses-setup/company-expenses-setup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-expenses-setup',
+  templateUrl: './company-expenses-setup.component.html',
+  styles: [
+  ]
+})
+export class CompanyExpensesSetupComponent {
+
+}
---
+++ stocky-web/src/app/routes/company/company-people/company-people-supplier/company-people-supplier.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-people-supplier',
+  templateUrl: './company-people-supplier.component.html',
+  styles: [
+  ]
+})
+export class CompanyPeopleSupplierComponent {
+
+}
