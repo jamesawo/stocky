7011755165dd6f1432811fbf3ee42e65690d021f
James Aworo <james.aworo@outlook.com>
2023-06-03 21:55:32 +0100

    update(components): update tax setup component, add form and display table

--- stocky-web/src/app/routes/company/company-tax-setup/company-tax-setup.component.ts
+++ stocky-web/src/app/routes/company/company-tax-setup/company-tax-setup.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TAX_SETUP} from '../../../data/constant/crumb.constant';
+import {Crumbs} from '../../../shared/components/breadcrumbs/breadcrumbs.component';
+import {ProductCategoryPayload} from '../../products/_data/product.payload';
 
 @Component({
-  selector: 'app-company-tax-setup',
-  templateUrl: './company-tax-setup.component.html',
-  styles: [
-  ]
+    selector: 'app-company-tax-setup',
+    templateUrl: './company-tax-setup.component.html',
+    styles: []
 })
 export class CompanyTaxSetupComponent {
+    public crumbs: Crumbs[] = TAX_SETUP;
+    public isExpanded = true;
+    public isLoading = false;
+    public isLoadingSearch = false;
+    public showModal = false;
+
+
+    public onOpenModal = () => {
+        this.showModal = true;
+    };
+
+    public onCloseModal = () => {
+        this.showModal = false;
+    };
+    public onSearchCategory = async () => {};
+
+    public onResetSearch = () => {};
+
+    public onCancelSearch = () => {};
+
+    public onCreate = async () => {};
+
+    public onCancelEdit = async (item: ProductCategoryPayload) => {};
+
+    public onToggleEdit = (item: ProductCategoryPayload) => {
+    };
+
+    public onSaveEdit = async (item: ProductCategoryPayload) => {};
+
+    public onConfirmDelete = async (id: number) => {};
+
+    public onCancelDelete = async () => {};
+
+    public onParentCategorySelected = (parent: ProductCategoryPayload) => {};
+
+    public onCacheValueChange = (change: any, item: ProductCategoryPayload, field: string) => {};
+
+    public canEditItem(item: any) {}
+
+    private onResetPayload() {}
 
 }
