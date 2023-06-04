eb7f4d9249bac31b78f96f97a91c8223afe639df
James Aworo <james.aworo@outlook.com>
2023-06-04 21:27:01 +0100

    update(data): update common types

---
+++ stocky-web/src/app/data/payload/common.types.ts
@@ -0,0 +1,14 @@
+import {NzButtonSize} from 'ng-zorro-antd/button';
+
+export type CommonInputProps = {
+    size?: NzButtonSize,
+    title?: string,
+    icon?: string,
+    showTable?: boolean,
+    showForm?: boolean,
+}
+
+export type CommonAddProps = {
+    showForm?: boolean,
+    showTable?: boolean
+}
