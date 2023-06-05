bd6c946a24cca9e4169aee66366ab2cfb412a4bd
James Aworo <james.aworo@outlook.com>
2023-06-05 23:17:49 +0100

    update(components): update stock routing and main modules

--- stocky-web/src/app/routes/stock/stock-routing.module.ts
+++ stocky-web/src/app/routes/stock/stock-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ManageStockComponent} from './manage-stock/manage-stock.component';
+import {MoveStockItemComponent} from './move-stock-item/move-stock-item.component';
 import {ReconcileStockComponent} from './reconcile-stock/reconcile-stock.component';
 import {RecordDamagedStockComponent} from './record-damaged-stock/record-damaged-stock.component';
 import {RecordStockBalanceComponent} from './record-stock-balance/record-stock-balance.component';
@@ -13,7 +14,8 @@ const routes: Routes = [
     {path: 'view-low-stock', component: ViewLowStockComponent},
     {path: 'reconcile-stock', component: ReconcileStockComponent},
     {path: 'record-stock-balance', component: RecordStockBalanceComponent},
-    {path: 'record-damaged-stock', component: RecordDamagedStockComponent}
+    {path: 'record-damaged-stock', component: RecordDamagedStockComponent},
+    {path: 'move-stock-items', component: MoveStockItemComponent}
 ];
 
 @NgModule({
