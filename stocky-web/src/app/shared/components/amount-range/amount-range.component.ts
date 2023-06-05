a36e513d8cc00ab3335194ab59917be28814199f
James Aworo <james.aworo@outlook.com>
2023-06-05 23:18:26 +0100

    update(components): add amount range component for amount filters

--- stocky-web/src/app/shared/components/amount-range/amount-range.component.ts
+++ stocky-web/src/app/shared/components/amount-range/amount-range.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-amount-range',
-  templateUrl: './amount-range.component.html',
-  styles: [
-  ]
+    selector: 'app-amount-range',
+    templateUrl: './amount-range.component.html',
+    styles: []
 })
 export class AmountRangeComponent {
+
+    public isAmountRange = true;
 
 }
