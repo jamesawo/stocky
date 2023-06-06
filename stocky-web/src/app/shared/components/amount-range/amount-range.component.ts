d18be6253ce8abe28758f70cb191ab7b604fa08e
James Aworo <james.aworo@outlook.com>
2023-06-06 15:00:09 +0100

    update(components): add shared amount-range and file-upload global components

--- stocky-web/src/app/shared/components/amount-range/amount-range.component.ts
+++ stocky-web/src/app/shared/components/amount-range/amount-range.component.ts
@@ -3,7 +3,11 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-amount-range',
     templateUrl: './amount-range.component.html',
-    styles: []
+    styles: [`
+      ::ng-deep .switch > nz-switch > button {
+        background-color: #475fd0;
+      }
+    `]
 })
 export class AmountRangeComponent {
 
--- stocky-web/src/app/shared/components/upload-file/upload-file.component.ts
+++ stocky-web/src/app/shared/components/upload-file/upload-file.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NzMessageService} from 'ng-zorro-antd/message';
+import {NzUploadChangeParam} from 'ng-zorro-antd/upload';
 
 @Component({
-  selector: 'app-upload-file',
-  templateUrl: './upload-file.component.html',
-  styles: [
-  ]
+    selector: 'app-upload-file',
+    templateUrl: './upload-file.component.html',
+    styles: []
 })
 export class UploadFileComponent {
+
+    constructor(private msg: NzMessageService) {}
+
+    handleChange({file, fileList}: NzUploadChangeParam): void {
+        const status = file.status;
+        if (status !== 'uploading') {
+            console.log(file, fileList);
+        }
+        if (status === 'done') {
+            this.msg.success(`${file.name} file uploaded successfully.`);
+        } else if (status === 'error') {
+            this.msg.error(`${file.name} file upload failed.`);
+        }
+    }
 
 }
