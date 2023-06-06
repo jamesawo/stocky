79d667baa6eae153f03c4c65ef852d548f145b77
James Aworo <james.aworo@outlook.com>
2023-06-06 14:48:41 +0100

    update(components): add button component to call up new creation form

--- stocky-web/src/app/routes/company/_components/company-expense-add-btn/company-expense-add-btn.component.ts
+++ stocky-web/src/app/routes/company/_components/company-expense-add-btn/company-expense-add-btn.component.ts
@@ -1,21 +1,37 @@
 import {Component, Input} from '@angular/core';
 import {UntypedFormGroup} from '@angular/forms';
 
+export type ExpensesAddBtnProps = {
+    display: 'modal' | 'drawer',
+    showTable?: boolean;
+    showForm?: boolean
+}
+
 @Component({
     selector: 'app-company-expense-add-btn',
     templateUrl: './company-expense-add-btn.component.html',
     styles: []
 })
 export class CompanyExpenseAddBtnComponent {
-
-    public isExpanded = true;
+    public showDrawer = false;
     public isLoading = false;
-    public isLoadingSearch = false;
     public showModal = false;
     public categoryForm!: UntypedFormGroup;
-
+    
     @Input()
-    public props: any;
+    public props: ExpensesAddBtnProps = {display: 'drawer'};
+
+    get isDrawer() {
+        return this.props.display == 'drawer';
+    }
+
+    public onOpenDrawer = () => {
+        this.showDrawer = true;
+    };
+
+    public onCloseDrawer = () => {
+        this.showDrawer = false;
+    };
 
     public onOpenModal = () => {
         this.showModal = true;
