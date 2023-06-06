5daf75ea6fee1a1e0fbf613affb418297bf746f2
James Aworo <james.aworo@outlook.com>
2023-06-06 14:49:32 +0100

    update(components): add company-expenses-category sub components

--- stocky-web/src/app/routes/company/_components/company-expense-category/company-expense-category-form/company-expense-category-form.component.ts
+++ stocky-web/src/app/routes/company/_components/company-expense-category/company-expense-category-form/company-expense-category-form.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, UntypedFormBuilder, Validators} from '@angular/forms';
+import {NzNotificationService} from 'ng-zorro-antd/notification';
+import {checkFormControlCharacterLimit, handleUsecaseRequest, isFormInvalid} from '../../../../../shared/utils/util';
+import {ExpenseCategoryUsecase} from '../../../_usecase/expense-category.usecase';
 
 @Component({
-  selector: 'app-company-expense-category-form',
-  templateUrl: './company-expense-category-form.component.html',
-  styles: [
-  ]
+    selector: 'app-company-expense-category-form',
+    templateUrl: './company-expense-category-form.component.html',
+    styles: []
 })
-export class CompanyExpenseCategoryFormComponent {
+export class CompanyExpenseCategoryFormComponent implements OnInit {
+
+    public form!: FormGroup;
+    public isSaving = false;
+
+    constructor(
+        private fb: UntypedFormBuilder,
+        private usecase: ExpenseCategoryUsecase,
+        private notification: NzNotificationService
+    ) {}
+
+    public ngOnInit() {
+        this.initForm();
+    }
+
+    public async onCreate(): Promise<void> {
+        const isInvalid = isFormInvalid(this.form);
+        if (isInvalid) {
+            return;
+        }
+        const option = this.form.value;
+        await handleUsecaseRequest(this.usecase.save(option), this.notification);
+        this.onResetPayload();
+    }
+
+    public checkCharacterLimit(value: any) {
+        const formControl = this.form.get('description');
+        checkFormControlCharacterLimit(formControl!);
+    }
+
+    private initForm() {
+        this.form = this.fb.group({
+            title: [null, [Validators.required]],
+            description: []
+        });
+    }
+
+    private onResetPayload() {
+        this.initForm();
+        this.isSaving = false;
+        this.usecase.setTrigger(true);
+    }
 
 }
--- stocky-web/src/app/routes/company/_components/company-expense-category/company-expense-category-table/company-expense-category-table.component.ts
+++ stocky-web/src/app/routes/company/_components/company-expense-category/company-expense-category-table/company-expense-category-table.component.ts
@@ -1,11 +1,86 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NzNotificationService} from 'ng-zorro-antd/notification';
+import {Observable, shareReplay} from 'rxjs';
+import {CommonPayload} from '../../../../../data/payload/common.payload';
+import {TableCol} from '../../../../../shared/components/table/table.component';
+import {EditCacheMap} from '../../../../../shared/components/update-delete-action/update-delete-action.component';
+import {
+    handleCancelEditingTableItem,
+    handleRemoveFromObservableListIfStatus,
+    handleUpdateObservableListIfResponse,
+    handleUsecaseRequest
+} from '../../../../../shared/utils/util';
+import {ExpenseCategoryUsecase} from '../../../_usecase/expense-category.usecase';
 
 @Component({
-  selector: 'app-company-expense-category-table',
-  templateUrl: './company-expense-category-table.component.html',
-  styles: [
-  ]
+    selector: 'app-company-expense-category-table',
+    templateUrl: './company-expense-category-table.component.html',
+    styles: []
 })
 export class CompanyExpenseCategoryTableComponent {
 
+    public cols: TableCol[] = [
+        {title: 'TITLE'},
+        {title: 'DESCRIPTION'},
+        {title: '', width: 20}
+    ];
+    public data?: Observable<any[]>;
+    public editMap: EditCacheMap<any> = {};
+    public paymentOptions?: Observable<CommonPayload[]>;
+
+    constructor(
+        private usecase: ExpenseCategoryUsecase,
+        private notification: NzNotificationService
+    ) {}
+
+    public ngOnInit() {
+        this.loadData();
+        this.usecase.trigger$.subscribe(value => this.loadData());
+    }
+
+    public loadData() {
+        this.paymentOptions = this.usecase.getAll().pipe(shareReplay());
+    }
+
+    public onConfirmDelete = async (id: number) => {
+        this.editMap[id] = {edit: false, data: {}, updating: false, deleting: true};
+        const response = await handleUsecaseRequest(this.usecase.remove(id), this.notification);
+        this.data = handleRemoveFromObservableListIfStatus(this.data!, {key: 'id', value: id}, response);
+        this.editMap[id].deleting = false;
+        this.notifyChange();
+    };
+
+    public onSaveEdit = async (category: CommonPayload) => {
+        this.editMap[category.id!].updating = true;
+        const data = this.editMap[category.id!].data;
+        const response = await handleUsecaseRequest(this.usecase.save(data), this.notification);
+        this.data = handleUpdateObservableListIfResponse(this.data!, response);
+        this.editMap[category.id!].edit = false;
+        this.notifyChange();
+    };
+
+    public onCancelDelete = async () => {};
+
+    public onCancelEdit = async (item: CommonPayload) => {
+        if (item) {
+            this.editMap = await handleCancelEditingTableItem(item as any, this.data!, this.editMap);
+        }
+    };
+
+    public onToggleEdit = (item: CommonPayload) => {
+        if (item) this.editMap[item.id!] = {edit: true, data: {...item}, updating: false, deleting: false};
+    };
+
+    public onCacheValueChange = (change: any, item: CommonPayload, field: string) => {
+        const data: any = this.editMap[item.id!].data;
+        data[field] = change;
+    };
+
+    public canEditItem(item: any): boolean {
+        return this.editMap[item.id] && this.editMap[item.id].edit;
+    }
+
+    private notifyChange = () => {
+        this.usecase.setTrigger(true);
+    };
 }
