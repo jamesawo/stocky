c2d47e661f8393a5aa94545639d15e003f24a211
James Aworo <james.aworo@outlook.com>
2023-06-06 14:50:33 +0100

    update(usecase): add expense-category business usecase for api endpoint integration

--- stocky-web/src/app/routes/company/_usecase/expense-category.usecase.ts
+++ stocky-web/src/app/routes/company/_usecase/expense-category.usecase.ts
@@ -2,32 +2,32 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {environment} from '@env/environment';
 import {BehaviorSubject, Observable} from 'rxjs';
-import {LocationPayload} from '../_data/company.payload';
+import {CommonPayload} from '../../../data/payload/common.payload';
 
 @Injectable({providedIn: 'root'})
-export class LocationUsecase {
+export class ExpenseCategoryUsecase {
 
     public trigger: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
     public trigger$: Observable<boolean> = this.trigger.asObservable();
 
-    private url = environment.api.baseUrl + '/company/location';
+    private url = environment.api.baseUrl + '/company/expenses-category';
 
     constructor(private http: HttpClient) {}
 
-    public save(payload: LocationPayload) {
-        return this.http.post<LocationPayload>(`${this.url}/create`, payload, {observe: 'response'});
+    public save(payload: CommonPayload) {
+        return this.http.post<CommonPayload>(`${this.url}/create`, payload, {observe: 'response'});
     }
 
     public getAll() {
-        return this.http.get<LocationPayload[]>(`${this.url}/get-all`);
+        return this.http.get<CommonPayload[]>(`${this.url}/get-all`);
     }
 
     public remove(id: number) {
         return this.http.delete(`${this.url}/remove/${id}`, {observe: 'response'});
     }
 
-    public update(payload: LocationPayload) {
-        return this.http.put<LocationPayload>(`${this.url}/update`, payload, {observe: 'response'});
+    public update(payload: CommonPayload) {
+        return this.http.put<CommonPayload>(`${this.url}/update`, payload, {observe: 'response'});
     }
 
     public setTrigger(value: boolean) {
