230bfdb83788f2e83a891b6fbfc343b37f0ece5d
James Aworo <james.aworo@outlook.com>
2023-06-08 18:59:26 +0100

    update(components): add manage people table, form and shared components

--- stocky-web/src/app/routes/company/company-people/company-people-employees/company-people-employees.component.ts
+++ stocky-web/src/app/routes/company/company-people/company-people-employees/company-people-employees.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {of} from 'rxjs';
+import {EMPLOYEE_SETUP} from '../../../../data/constant/crumb.constant';
+import {TableCol} from '../../../../shared/components/table/table.component';
 
 @Component({
-  selector: 'app-company-people-employees',
-  templateUrl: './company-people-employees.component.html',
-  styles: [
-  ]
+    selector: 'app-company-people-employees',
+    templateUrl: './company-people-employees.component.html',
+    styles: []
 })
 export class CompanyPeopleEmployeesComponent {
+
+    public crumbs = EMPLOYEE_SETUP;
+    public isOpenHeader = true;
+    public isLoading = false;
+    public list = of(Array(1));
+
+
+    public tableCols: TableCol[] = [
+        {title: 'Employee #'},
+        {title: 'Full Name'},
+        {title: 'Email'},
+        {title: 'Phone'},
+        {title: 'Date Registered'},
+        {title: 'Expiration Date'},
+        {title: 'Role'},
+        {title: 'Action'}
+    ];
+
+    public onSearch = async (): Promise<void> => {};
+
+    public onReset = (): void => {};
+
+    public onCancel = (): void => {};
 
 }
