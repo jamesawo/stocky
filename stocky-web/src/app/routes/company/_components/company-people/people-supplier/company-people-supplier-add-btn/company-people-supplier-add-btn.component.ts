83003dc19e76c3ae20aa9a1b1ea69d4c8f32e4aa
James Aworo <james.aworo@outlook.com>
2023-06-09 14:32:15 +0100

    update(components): update suppliers component

---
+++ stocky-web/src/app/routes/company/_components/company-people/people-supplier/company-people-supplier-add-btn/company-people-supplier-add-btn.component.ts
@@ -0,0 +1,51 @@
+import {Component, Input} from '@angular/core';
+import {UntypedFormGroup} from '@angular/forms';
+import {CommonAddProps, PopupViewProps} from 'src/app/data/payload/common.types';
+
+export type ExpensesAddBtnProps = {
+    showTable?: boolean;
+    showForm?: boolean
+}
+
+@Component({
+    selector: 'app-company-people-supplier-add-btn',
+    templateUrl: './company-people-supplier-add-btn.component.html',
+    styles: []
+})
+export class CompanyPeopleSupplierAddBtnComponent {
+    public showDrawer = false;
+    public isLoading = false;
+    public showModal = false;
+    public categoryForm!: UntypedFormGroup;
+
+    @Input()
+    public props: CommonAddProps = {};
+
+    @Input()
+    public popup: PopupViewProps = {display: 'drawer'};
+    public pageTitle = 'Add New Customer';
+
+    get isDrawer() {
+        return this.popup.display == 'drawer';
+    }
+
+    public onOpenDrawer = () => {
+        this.showDrawer = true;
+    };
+
+    public onCloseDrawer = () => {
+        this.showDrawer = false;
+    };
+
+    public onOpenModal = () => {
+        this.showModal = true;
+    };
+
+    public onCloseModal = () => {
+        this.showModal = false;
+    };
+
+    public onCreate = () => {};
+
+
+}
