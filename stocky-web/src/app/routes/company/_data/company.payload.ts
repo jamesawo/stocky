d303839d4f7f4e506383b32ef3d5d8722687e4af
James Aworo <james.aworo@outlook.com>
2023-06-09 14:29:13 +0100

    update(data): add user payload

--- stocky-web/src/app/routes/company/_data/company.payload.ts
+++ stocky-web/src/app/routes/company/_data/company.payload.ts
@@ -1,3 +1,5 @@
+import {ProductCategoryPayload} from '../../products/_data/product.payload';
+import {RoleUsecase} from '../_usecase/role.usecase';
 import {LocationTypeEnum} from './company.enum';
 
 export class CompanyPayload {
@@ -16,4 +18,43 @@ export class RolePayload {
     title?: string;
     description?: string;
     permissions?: any[];
+}
+
+export class UserBasicDetailsPayload {
+}
+
+export class UserAccountDetailsPayload {
+    id?: number;
+    username?: string;
+    password?: string;
+    role?: RoleUsecase;
+}
+
+export class UserEmergencyDetails {
+    nokFullName?: string;
+    nokPhone?: string;
+    nokEmail?: string;
+    nokAddress?: string;
+
+}
+
+
+export class UserPayload {
+    id?: number;
+    basicDetails?: UserBasicDetailsPayload;
+    accountDetails?: UserAccountDetailsPayload;
+    emergencyDetails?: UserEmergencyDetails;
+}
+
+
+export class SupplierPayload {
+    id?: number;
+    fullName?: string;
+    businessName?: string;
+    phoneNumber?: string;
+    email?: string;
+    address?: string;
+    productCategories: ProductCategoryPayload[] = [];
+    date?: string;
+    recordedBy?: string;
 }
