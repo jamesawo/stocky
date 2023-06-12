5c8be85da071ac46ca86ec545ca1d7434b9e4985
James Aworo <james.aworo@outlook.com>
2023-06-12 13:54:54 +0100

    update(components): update report pages, add comingsoon display

--- stocky-web/src/app/routes/report/report-routing.module.ts
+++ stocky-web/src/app/routes/report/report-routing.module.ts
@@ -1,12 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {ReportCustomerComponent} from './report-customer/report-customer.component';
+import {ReportEmployeeComponent} from './report-employee/report-employee.component';
+import {ReportExpenseComponent} from './report-expense/report-expense.component';
+import {ReportSalesComponent} from './report-sales/report-sales.component';
+import {ReportStockComponent} from './report-stock/report-stock.component';
 
 const routes: Routes = [
-    {path: 'sales-report', component: undefined},
-    {path: 'expense-report', component: undefined},
-    {path: 'employee-report', component: undefined},
-    {path: 'customer-report', component: undefined},
-    {path: 'stock-report', component: undefined}
+    {path: 'sales-report', component: ReportSalesComponent},
+    {path: 'expenses-report', component: ReportExpenseComponent},
+    {path: 'employees-report', component: ReportEmployeeComponent},
+    {path: 'customers-report', component: ReportCustomerComponent},
+    {path: 'stock-report', component: ReportStockComponent}
 ];
 
 @NgModule({
