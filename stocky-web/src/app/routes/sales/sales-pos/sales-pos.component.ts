175233203916c106a1b845ecf30e0ac9cd60c71a
James Aworo <james.aworo@outlook.com>
2023-06-12 13:55:34 +0100

    update(components): add sales pages with comingsoon display

---
+++ stocky-web/src/app/routes/sales/sales-pos/sales-pos.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-sales-pos',
+    templateUrl: './sales-pos.component.html',
+    styles: []
+})
+export class SalesPosComponent {
+
+}
---
+++ stocky-web/src/app/routes/sales/sales-refund/sales-refund.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-refund',
+  templateUrl: './sales-refund.component.html',
+  styles: [
+  ]
+})
+export class SalesRefundComponent {
+
+}
