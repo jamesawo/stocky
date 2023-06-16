dc36c609dbf187ff493f31bbd7482dfcfe663ccc
James Aworo <james.aworo@outlook.com>
2023-06-16 23:44:22 +0100

    update(payload): refactor company payload, remove redundant classes

--- stocky-web/src/app/routes/company/_data/company-setup.payload.ts
+++ stocky-web/src/app/routes/company/_data/company-setup.payload.ts
@@ -1,30 +1,18 @@
-import {CommonPayload} from '../../../data/payload/common.payload';
-
 export class CompanySetupPayload {
-}
-
-export class CompanyBasicDetailsPayload {
     id?: number;
-    businessName?: string;
-    businessCategory?: CommonPayload;
-    businessEmployeeSize?: CommonPayload;
-    businessNumberOfYearsOfOperation?: number;
-    businessNumberOfBranch?: number;
-    businessAddress?: string;
+    setupKey?: string;
+    setupValue?: string;
+
+    constructor(key: string, value: string) {
+        this.setupKey = key;
+        this.setupValue = value;
+    }
 }
 
-
-export class CompanyAdministratorDetailsPayload {
-    id?: number;
-    profileName?: string;
-    profilePhone?: string;
-    profileEmail?: string;
-    profilePosition?: string;
-}
 
 export class CompanyLocaleDetailsPayload {
     id?: number;
-    
+
     currency?: string;
     language?: string;
     timeZone?: string;
