ad28d4e247515c636aedced60e94b124dc0ca1e9
James Aworo <james.aworo@outlook.com>
2023-06-16 23:49:37 +0100

    update(components): clean up business category dropdown component

--- stocky-web/src/app/shared/components/business-category-dropdown/business-category-dropdown.component.ts
+++ stocky-web/src/app/shared/components/business-category-dropdown/business-category-dropdown.component.ts
@@ -1,40 +1,18 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {NzSelectOptionInterface} from 'ng-zorro-antd/select';
+import {Observable, of, shareReplay} from 'rxjs';
 import {CommonPayload} from '../../../data/payload/common.payload';
 import {FormProps} from '../../../data/payload/common.types';
+import {BasicSetupUsecase} from '../../../routes/company/_usecase/company-setup/basic-setup.usecase';
+import {getNzFormControlValidStatus} from '../../utils/util';
 
-
-const businessIndustryTypes: CommonPayload[] = [
-    {id: 1, title: 'Beauty Salon'},
-    {id: 2, title: 'Bakery'},
-    {id: 3, title: 'Supermarket'},
-    {id: 4, title: 'Restaurant'},
-    {id: 5, title: 'Clothing Store'},
-    {id: 6, title: 'Pharmacy'},
-    {id: 7, title: 'Hardware Store'},
-    {id: 8, title: 'Jewelry Store'},
-    {id: 9, title: 'Fitness Center'},
-    {id: 10, title: 'Electronics Store'},
-    {id: 11, title: 'Bookstore'},
-    {id: 12, title: 'Coffee Shop'},
-    {id: 13, title: 'Pet Store'},
-    {id: 14, title: 'Toy Store'},
-    {id: 15, title: 'Music Store'},
-    {id: 16, title: 'Home Decor Store'},
-    {id: 17, title: 'Garden Center'},
-    {id: 18, title: 'Auto Repair Shop'},
-    {id: 19, title: 'Dental Clinic'},
-    {id: 20, title: 'Gift Shop'},
-    {id: 21, title: 'Tyre Store'},
-    {id: 22, title: 'Other'}
-];
 
 @Component({
     selector: 'app-business-category-dropdown',
     templateUrl: './business-category-dropdown.component.html',
     styles: []
 })
-export class BusinessCategoryDropdownComponent {
+export class BusinessCategoryDropdownComponent implements OnInit {
 
     @Input() public formProps?: FormProps;
 
@@ -43,8 +21,14 @@ export class BusinessCategoryDropdownComponent {
     @Output()
     public valueChange = new EventEmitter<CommonPayload | NzSelectOptionInterface>();
 
-    public list = businessIndustryTypes.map(item => { return {'label': item.title!, 'value': item}; });
+    public businessCategories: Observable<CommonPayload[]> = of([]);
+    protected readonly getNzFormControlValidStatus = getNzFormControlValidStatus;
 
+    constructor(private usecase: BasicSetupUsecase) {}
+
+    public ngOnInit() {
+        this.businessCategories = this.usecase.getBusinessCategories().pipe(shareReplay());
+    }
 
     public onModelChange(selected: CommonPayload | NzSelectOptionInterface) {
         if (selected) {
