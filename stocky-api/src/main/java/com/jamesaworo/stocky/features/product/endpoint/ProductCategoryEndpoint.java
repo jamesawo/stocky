d8e2959b674406bbd959b6e559b5950a5d4895fc
James Aworo <james.aworo@outlook.com>
2023-06-20 13:57:44 +0100

    update(endpoint): add api endpoint for product-category update status

--- stocky-api/src/main/java/com/jamesaworo/stocky/features/product/endpoint/ProductCategoryEndpoint.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/product/endpoint/ProductCategoryEndpoint.java
@@ -20,40 +20,48 @@ import static com.jamesaworo.stocky.core.constants.Global.API_PREFIX;
 @RequestMapping(value = API_PREFIX + "/product-category")
 @RequiredArgsConstructor
 public class ProductCategoryEndpoint {
-    private final IProductCategoryInteractor interactor;
+	private final IProductCategoryInteractor interactor;
 
-    @GetMapping(value = "get/{id}")
-    public ResponseEntity<ProductCategoryRequest> find(@PathVariable Long id) {
-        return this.interactor.find(id);
-    }
+	@GetMapping(value = "get/{id}")
+	public ResponseEntity<ProductCategoryRequest> find(@PathVariable Long id) {
+		return this.interactor.find(id);
+	}
 
-    @GetMapping("all")
-    public ResponseEntity<List<ProductCategoryRequest>> findMany() {
-        return this.interactor.findMany();
-    }
+	@GetMapping("all")
+	public ResponseEntity<List<ProductCategoryRequest>> findMany() {
+		return this.interactor.findMany();
+	}
 
-    @PostMapping("create")
-    public ResponseEntity<Optional<ProductCategoryRequest>> save(
-            @RequestBody @Valid ProductCategoryRequest dto) {
-        return this.interactor.save(dto);
-    }
+	@PostMapping("create")
+	public ResponseEntity<Optional<ProductCategoryRequest>> save(
+			@RequestBody @Valid ProductCategoryRequest dto
+	) {
+		return this.interactor.save(dto);
+	}
 
-    @PutMapping("update")
-    public ResponseEntity<Optional<ProductCategoryRequest>> update(
-            @RequestBody @Valid ProductCategoryRequest dto
-    ) {
-        return this.interactor.update(dto);
-    }
+	@PutMapping("update")
+	public ResponseEntity<Optional<ProductCategoryRequest>> update(
+			@RequestBody @Valid ProductCategoryRequest dto
+	) {
+		return this.interactor.update(dto);
+	}
 
-    @DeleteMapping("delete/{id}")
-    public ResponseEntity<Optional<Boolean>> remove(@PathVariable Long id) {
-        return this.interactor.remove(id);
-    }
+	@DeleteMapping("delete/{id}")
+	public ResponseEntity<Optional<Boolean>> remove(@PathVariable Long id) {
+		return this.interactor.remove(id);
+	}
 
-    @GetMapping("search")
-    public ResponseEntity<List<ProductCategoryRequest>> search(
-            @RequestParam(value = "term") String term) {
-        return this.interactor.search(term);
-    }
+	@GetMapping("search")
+	public ResponseEntity<List<ProductCategoryRequest>> search(
+			@RequestParam(value = "term") String term
+	) {
+		return this.interactor.search(term);
+	}
+
+	@PutMapping("status/{id}")
+	public ResponseEntity<Optional<Boolean>> toggleIsActiveStatus(@PathVariable Long id) {
+		return this.interactor.toggleActiveStatus(id);
+	}
+
 
 }
