9460baa2e2734ab8a1a3d403dc2a4b3da6a1d106
James Aworo <james.aworo@outlook.com>
2023-06-28 23:33:20 +0100

    update(converter): check date source isnullable before parsing date string

--- stocky-api/src/main/java/com/jamesaworo/stocky/configuration/converter/LocalDateStringConverter.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/configuration/converter/LocalDateStringConverter.java
@@ -8,6 +8,7 @@
 package com.jamesaworo.stocky.configuration.converter;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.util.ObjectUtils;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -17,6 +18,6 @@ public class LocalDateStringConverter implements Converter<String, LocalDate> {
 	@Override
 	public LocalDate convert(String source) {
 		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		return LocalDate.parse(source, format);
+		return ObjectUtils.isEmpty(source) ? null : LocalDate.parse(source, format);
 	}
 }
