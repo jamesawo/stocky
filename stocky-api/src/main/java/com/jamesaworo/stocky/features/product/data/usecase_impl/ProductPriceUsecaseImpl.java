76faabdabd795bd7f431e97ae44439cecf579f88
James Aworo <james.aworo@outlook.com>
2023-07-05 12:01:08 +0100

    update(usecase): refactor product usecase, simplify update product quantity after saving new stock

--- stocky-api/src/main/java/com/jamesaworo/stocky/features/product/data/usecase_impl/ProductPriceUsecaseImpl.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/product/data/usecase_impl/ProductPriceUsecaseImpl.java
@@ -9,17 +9,57 @@ package com.jamesaworo.stocky.features.product.data.usecase_impl;
 
 import com.jamesaworo.stocky.core.annotations.Usecase;
 import com.jamesaworo.stocky.features.product.data.repository.ProductPriceRepository;
+import com.jamesaworo.stocky.features.product.domain.entity.Product;
 import com.jamesaworo.stocky.features.product.domain.entity.ProductPrice;
 import com.jamesaworo.stocky.features.product.domain.usecase.IProductPriceUsecase;
+import com.jamesaworo.stocky.features.product.domain.usecase.IProductUsecase;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
 
 @Usecase
 @RequiredArgsConstructor
 public class ProductPriceUsecaseImpl implements IProductPriceUsecase {
 	private final ProductPriceRepository repository;
+	private final IProductUsecase productUsecase;
+
 
 	public ProductPrice save(ProductPrice price) {
 		return this.repository.save(price);
+	}
+
+	@Override
+	public ProductPrice updateProductPrice(Product product, ProductPrice price) {
+		Optional<Product> optionalProduct = this.productUsecase.findById(product.getId());
+		optionalProduct.ifPresent((existingProduct) -> this.createOrUpdateProductPrice(existingProduct, price));
+		return price;
+	}
+
+	private void createOrUpdateProductPrice(Product existingProduct, ProductPrice price) {
+		ProductPrice existingProductPrice = existingProduct.getPrice();
+
+		if (existingProductPrice != null) {
+			existingProductPrice.setCostPrice(price.getCostPrice());
+			existingProductPrice.setMarkup(price.getMarkup());
+			existingProductPrice.setSellingPrice(price.getSellingPrice());
+			ProductPrice savedProductPrice = this.save(existingProductPrice);
+			this.setProductPriceAndUpdateProduct(existingProduct, savedProductPrice);
+		}
+		else {
+			ProductPrice savedProductPrice = this.save(price);
+			this.setProductPriceAndUpdateProduct(existingProduct, savedProductPrice);
+			price.setId(savedProductPrice.getId());
+		}
+	}
+
+	private void setProductPriceAndUpdateProduct(Product existingProduct, ProductPrice savedProductPrice) {
+		existingProduct.setPrice(savedProductPrice);
+		this.productUsecase.save(existingProduct);
+	}
+
+	@Override
+	public Optional<ProductPrice> findPriceByProduct(Product product) {
+		return this.repository.findProductPriceByProduct(product);
 	}
 
 }
--- stocky-api/src/main/java/com/jamesaworo/stocky/features/product/data/usecase_impl/ProductUsecaseImpl.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/product/data/usecase_impl/ProductUsecaseImpl.java
@@ -3,6 +3,8 @@ package com.jamesaworo.stocky.features.product.data.usecase_impl;
 import com.jamesaworo.stocky.core.annotations.Usecase;
 import com.jamesaworo.stocky.features.product.data.repository.ProductRepository;
 import com.jamesaworo.stocky.features.product.domain.entity.Product;
+import com.jamesaworo.stocky.features.product.domain.entity.ProductBasic;
+import com.jamesaworo.stocky.features.product.domain.usecase.IProductBasicUsecase;
 import com.jamesaworo.stocky.features.product.domain.usecase.IProductUsecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -12,6 +14,8 @@ import org.springframework.data.jpa.domain.Specification;
 import java.util.List;
 import java.util.Optional;
 
+import static com.jamesaworo.stocky.features.product.domain.enums.ProductQuantityUpdateType.INCREMENT;
+
 /**
  * @author Aworo James
  * @since 5/10/23
@@ -20,25 +24,36 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ProductUsecaseImpl implements IProductUsecase {
 
-    private final ProductRepository repository;
+	private final ProductRepository repository;
+	private final IProductBasicUsecase basicUsecase;
 
-    @Override
-    public Optional<Product> findById(Long id) {
-        return this.repository.findById(id);
-    }
+	@Override
+	public Optional<Product> findById(Long id) {
+		return this.repository.findById(id);
+	}
 
-    @Override
-    public Product save(Product product) {
-        return this.repository.save(product);
-    }
+	@Override
+	public Product save(Product product) {
+		return this.repository.save(product);
+	}
 
-    @Override
-    public Page<Product> findMany(Specification<Product> specification, Pageable pageable) {
-        return this.repository.findAll(specification, pageable);
-    }
+	@Override
+	public Page<Product> findMany(Specification<Product> specification, Pageable pageable) {
+		return this.repository.findAll(specification, pageable);
+	}
 
-    @Override
-    public List<Product> findMany(Specification<Product> specification) {
-        return this.repository.findAll(specification);
-    }
+	@Override
+	public List<Product> findMany(Specification<Product> specification) {
+		return this.repository.findAll(specification);
+	}
+
+	@Override
+	public Boolean updateProductQuantity(Product product, Integer quantity) {
+		Optional<Product> optionalProduct = this.findById(product.getId());
+		return optionalProduct.map(product1 -> {
+			ProductBasic basic = product1.getBasic();
+			int result = this.basicUsecase.updateProductQuantity(basic.getId(), quantity, INCREMENT);
+			return result == 1;
+		}).orElse(Boolean.FALSE);
+	}
 }
