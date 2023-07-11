f8f4ab45e4144e8533a68d2dc214907a0179d643
James Aworo <james.aworo@outlook.com>
2023-07-11 21:42:53 +0100

    update(endpoint): udpate stock-create route to return a stock request json object

--- stocky-api/src/main/java/com/jamesaworo/stocky/features/stock/endpoint/StockEndpoint.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/stock/endpoint/StockEndpoint.java
@@ -27,24 +27,24 @@ import static com.jamesaworo.stocky.core.constants.Global.API_PREFIX;
 @RequiredArgsConstructor
 public class StockEndpoint {
 
-	private final IStockInteractor interactor;
+    private final IStockInteractor interactor;
 
-	@PostMapping(value = "/create")
-	public ResponseEntity<Boolean> create(@RequestBody StockRequest request) {
-		return this.interactor.save(request);
-	}
+    @PostMapping(value = "/create")
+    public ResponseEntity<StockRequest> create(@RequestBody StockRequest request) {
+        return this.interactor.save(request);
+    }
 
-	@PostMapping(value = "/search-request")
-	public ResponseEntity<PageSearchResult<List<StockRequest>>> searchProducts(
-			@Valid @RequestBody PageSearchRequest<StockSearchRequest> request
-	) {
-		return this.interactor.search(request);
-	}
+    @PostMapping(value = "/search-request")
+    public ResponseEntity<PageSearchResult<List<StockRequest>>> searchProducts(
+            @Valid @RequestBody PageSearchRequest<StockSearchRequest> request
+    ) {
+        return this.interactor.search(request);
+    }
 
-	@GetMapping("search")
-	public ResponseEntity<List<StockRequest>> search(
-			@RequestParam(value = "term") String term
-	) {
-		return this.interactor.search(term);
-	}
+    @GetMapping("search")
+    public ResponseEntity<List<StockRequest>> search(
+            @RequestParam(value = "term") String term
+    ) {
+        return this.interactor.search(term);
+    }
 }
