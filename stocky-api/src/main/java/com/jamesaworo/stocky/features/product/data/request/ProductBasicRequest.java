5d55f988f499be5120e9ffb7588ed3644e32bfb1
James Aworo <james.aworo@outlook.com>
2023-07-14 00:36:49 +0100

    update(product): add product quanity to product basic request

--- stocky-api/src/main/java/com/jamesaworo/stocky/features/product/data/request/ProductBasicRequest.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/product/data/request/ProductBasicRequest.java
@@ -21,20 +21,21 @@ import java.util.List;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProductBasicRequest {
-	private Long id;
-	private ProductCategoryRequest productCategory;
-	private ProductUnitOfMeasureRequest unitOfMeasure;
-	private ProductStatusRequest status;
-	private Boolean isActive;
-	private Boolean useQuantity;
-	private Boolean isService;
-	private Integer minAgeLimit;
-	private String productName;
-	private String brandName;
-	private String sku;
-	private String barcode;
-	private String description;
-	private Integer lowStockPoint;
-	private List<ProductTaxRequest> taxes;
+    private Long id;
+    private ProductCategoryRequest productCategory;
+    private ProductUnitOfMeasureRequest unitOfMeasure;
+    private ProductStatusRequest status;
+    private Boolean isActive;
+    private Boolean useQuantity;
+    private Boolean isService;
+    private Integer minAgeLimit;
+    private String productName;
+    private String brandName;
+    private String sku;
+    private String barcode;
+    private String description;
+    private Integer lowStockPoint;
+    private List<ProductTaxRequest> taxes;
+    private Integer quantity;
 
 }
