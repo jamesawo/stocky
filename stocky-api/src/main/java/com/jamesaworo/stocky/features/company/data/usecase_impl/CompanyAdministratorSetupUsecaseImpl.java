6dc48617c39eeef5b412a615b5918b26689f96ab
James Aworo <james.aworo@outlook.com>
2023-07-15 23:47:13 +0100

    update(company-usecase): add company admin setup as settings with key-value pairs

--- stocky-api/src/main/java/com/jamesaworo/stocky/features/company/data/usecase_impl/CompanyAdministratorSetupUsecaseImpl.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/company/data/usecase_impl/CompanyAdministratorSetupUsecaseImpl.java
@@ -11,6 +11,7 @@ import com.jamesaworo.stocky.core.annotations.Usecase;
 import com.jamesaworo.stocky.features.company.data.repository.CompanyAdministratorDetailRepository;
 import com.jamesaworo.stocky.features.company.domain.entity.CompanyAdministratorDetail;
 import com.jamesaworo.stocky.features.company.domain.usecase.ICompanyAdministratorSetupUsecase;
+import com.jamesaworo.stocky.features.settings.domain.entity.Setting;
 import lombok.RequiredArgsConstructor;
 
 import javax.transaction.Transactional;
@@ -21,32 +22,39 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CompanyAdministratorSetupUsecaseImpl implements ICompanyAdministratorSetupUsecase {
 
-	private final CompanyAdministratorDetailRepository repository;
+    private final CompanyAdministratorDetailRepository repository;
 
-	@Override
-	public List<CompanyAdministratorDetail> all() {
-		return this.repository.findAll();
-	}
+    @Override
+    public List<CompanyAdministratorDetail> all() {
+        return this.repository.findAll();
+    }
 
-	@Override
-	public Optional<CompanyAdministratorDetail> get(String key) {
-		return this.repository.findBySetupKeyEqualsIgnoreCase(key);
-	}
+    @Override
+    public Optional<CompanyAdministratorDetail> get(String key) {
+        return this.repository.findBySetupKeyEqualsIgnoreCase(key);
+    }
 
-	@Override
-	public Optional<Boolean> update(String key, String value) {
-		Optional<CompanyAdministratorDetail> optional = this.get(key);
-		return optional.map(detail -> {
-			int count = this.repository.updateValueWhereKey(value, key);
-			return count == 1;
-		});
-	}
+    @Override
+    public Setting getAsSetting(String key) {
+        Optional<CompanyAdministratorDetail> optional = this.get(key);
+        return optional.map(admin -> new Setting(admin.getSetupKey(), admin.getSetupValue())).orElse(new Setting());
+    }
 
-	@Override
-	@Transactional
-	public void updateMany(List<CompanyAdministratorDetail> list) {
-		list.forEach(basicDetail -> {
-			this.update(basicDetail.getSetupKey(), basicDetail.getSetupValue());
-		});
-	}
+
+    @Override
+    public Optional<Boolean> update(String key, String value) {
+        Optional<CompanyAdministratorDetail> optional = this.get(key);
+        return optional.map(detail -> {
+            int count = this.repository.updateValueWhereKey(value, key);
+            return count == 1;
+        });
+    }
+
+    @Override
+    @Transactional
+    public void updateMany(List<CompanyAdministratorDetail> list) {
+        list.forEach(basicDetail -> {
+            this.update(basicDetail.getSetupKey(), basicDetail.getSetupValue());
+        });
+    }
 }
