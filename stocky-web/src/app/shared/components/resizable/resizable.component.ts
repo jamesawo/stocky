25bcf55c3f8db4990a05765ee041c18d77ba5d9a
James Aworo <james.aworo@outlook.com>
2023-07-17 12:07:55 +0100

    update(shared-component): add resizable base component

---
+++ stocky-web/src/app/shared/components/resizable/resizable.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resizable',
+  templateUrl: './resizable.component.html',
+  styles: [
+  ]
+})
+export class ResizableComponent {
+
+}
