fa3fcbe2daffca8d02cfcd235b7028555a4d473b
James Aworo <james.aworo@outlook.com>
2023-07-18 21:35:36 +0100

    update(data): add implementation for customer search using a search term

--- stocky-api/src/main/java/com/jamesaworo/stocky/features/company/data/interactor/implementation/CompanyCustomerInteractorImpl.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/company/data/interactor/implementation/CompanyCustomerInteractorImpl.java
@@ -34,45 +34,52 @@ import static org.springframework.http.ResponseEntity.ok;
 @Interactor
 @RequiredArgsConstructor
 public class CompanyCustomerInteractorImpl implements ICompanyCustomerInteractor, Mapper<CompanyCustomerRequest, CompanyCustomer> {
-	private final ICompanyCustomerUsecase usecase;
-	private final ModelMapper modelMapper;
+    private final ICompanyCustomerUsecase usecase;
+    private final ModelMapper modelMapper;
 
-	@Override
-	public CompanyCustomerRequest toRequest(CompanyCustomer model) {
-		return this.modelMapper.map(model, CompanyCustomerRequest.class);
-	}
+    @Override
+    public CompanyCustomerRequest toRequest(CompanyCustomer model) {
+        return this.modelMapper.map(model, CompanyCustomerRequest.class);
+    }
 
-	@Override
-	public CompanyCustomer toModel(CompanyCustomerRequest request) {
-		return this.modelMapper.map(request, CompanyCustomer.class);
-	}
+    @Override
+    public CompanyCustomer toModel(CompanyCustomerRequest request) {
+        return this.modelMapper.map(request, CompanyCustomer.class);
+    }
 
-	@Override
-	public ResponseEntity<PageSearchResult<List<CompanyCustomerRequest>>> search(PageSearchRequest<CompanyCustomerSearchRequest> request) {
-		Page<CompanyCustomer> page = this.usecase.findMany(
-				companyCustomerSpecification(request.getSearchRequest()),
-				request.getPage().toPageable()
-		);
-		Set<CompanyCustomer> customers = new HashSet<>(page.getContent());
-		List<CompanyCustomerRequest> requests = customers.stream().map(this::toRequest).collect(Collectors.toList());
-		return ok().body(toPageSearchResult(requests, page));
-	}
+    @Override
+    public ResponseEntity<PageSearchResult<List<CompanyCustomerRequest>>> search(PageSearchRequest<CompanyCustomerSearchRequest> request) {
+        Page<CompanyCustomer> page = this.usecase.findMany(
+                companyCustomerSpecification(request.getSearchRequest()),
+                request.getPage().toPageable()
+        );
+        Set<CompanyCustomer> customers = new HashSet<>(page.getContent());
+        List<CompanyCustomerRequest> requests = customers.stream().map(this::toRequest).collect(Collectors.toList());
+        return ok().body(toPageSearchResult(requests, page));
+    }
 
-	@Override
-	public ResponseEntity<CompanyCustomerRequest> save(CompanyCustomerRequest customer) {
-		CompanyCustomer savedCustomer = this.usecase.save(toModel(customer));
-		return ok().body(toRequest(savedCustomer));
-	}
+    @Override
+    public List<CompanyCustomerRequest> search(String term) {
+        List<CompanyCustomer> page = this.usecase.findMany(companyCustomerSpecification(term));
+        Set<CompanyCustomer> customers = new HashSet<>(page);
+        return customers.stream().map(this::toRequest).collect(Collectors.toList());
+    }
 
-	@Override
-	public ResponseEntity<Optional<Boolean>> update(CompanyCustomerRequest customer) {
-		Optional<Boolean> optionalBoolean = this.usecase.update(toModel(customer));
-		return ok().body(optionalBoolean);
-	}
+    @Override
+    public ResponseEntity<CompanyCustomerRequest> save(CompanyCustomerRequest customer) {
+        CompanyCustomer savedCustomer = this.usecase.save(toModel(customer));
+        return ok().body(toRequest(savedCustomer));
+    }
 
-	@Override
-	public ResponseEntity<Optional<Boolean>> toggleActiveStatus(Long id) {
-		Optional<Boolean> optional = this.usecase.toggleActiveStatus(id);
-		return ok().body(optional);
-	}
+    @Override
+    public ResponseEntity<Optional<Boolean>> update(CompanyCustomerRequest customer) {
+        Optional<Boolean> optionalBoolean = this.usecase.update(toModel(customer));
+        return ok().body(optionalBoolean);
+    }
+
+    @Override
+    public ResponseEntity<Optional<Boolean>> toggleActiveStatus(Long id) {
+        Optional<Boolean> optional = this.usecase.toggleActiveStatus(id);
+        return ok().body(optional);
+    }
 }
