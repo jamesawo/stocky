db0f5b1e6f7513540d4d33dfb632e43376fcccb6
James Aworo <james.aworo@outlook.com>
2023-07-18 21:38:26 +0100

    update(data): add criterial api specification function for customer search

--- stocky-api/src/main/java/com/jamesaworo/stocky/features/company/data/request/specification/CompanyCustomerSearchSpecification.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/company/data/request/specification/CompanyCustomerSearchSpecification.java
@@ -22,53 +22,66 @@ import java.util.List;
 import static com.jamesaworo.stocky.core.predicates.SearchPredicates.activeRecordPredicate;
 import static com.jamesaworo.stocky.core.predicates.SearchPredicates.dateRangeParamPredicates;
 import static org.springframework.util.ObjectUtils.isEmpty;
+import static org.springframework.util.StringUtils.hasText;
 
 @Component
 public class CompanyCustomerSearchSpecification {
 
-	private static Root<CompanyCustomer> mainRoot;
+    private static Root<CompanyCustomer> mainRoot;
 
-	public static Specification<CompanyCustomer> companyCustomerSpecification(CompanyCustomerSearchRequest request) {
-		return (root, criteriaQuery, criteriaBuilder) -> {
-
-			mainRoot = root;
-			List<Predicate> predicates = new ArrayList<>();
-
-			// customer full name
-			if (!isEmpty(request) && !isEmpty(request.getCustomerFullName())) {
-				predicates.addAll(fullNamePredicates(criteriaBuilder, request.getCustomerFullName()));
-			}
-
-			// customer phone
-			if (!isEmpty(request) && !isEmpty(request.getCustomerPhoneNumber())) {
-				predicates.add(phoneNumberPredicate(criteriaBuilder, request.getCustomerPhoneNumber()));
-			}
-
-			// customer email
-			if (!isEmpty(request) && !isEmpty(request.getCustomerEmail())) {
-				predicates.add(emailPredicate(criteriaBuilder, request.getCustomerEmail()));
-			}
-
-			// date range
-			if (!isEmpty(request.getDateRangeParam())) {
-				predicates.addAll(dateRangeParamPredicates(criteriaBuilder, request.getDateRangeParam(), mainRoot));
-			}
-
-			// customer tag
-			if (!isEmpty(request) && !isEmpty(request.getCustomerTag())) {
-				predicates.add(customerTagPredicate(criteriaBuilder, request.getCustomerTag()));
-			}
+    public static Specification<CompanyCustomer> companyCustomerSpecification(String term) {
+        return (root, criteriaQuery, criteriaBuilder) -> {
+            mainRoot = root;
+            List<Predicate> predicates = new ArrayList<>();
+            if (!isEmpty(term) && hasText(term)) {
+                predicates.addAll(nameEmailPhoneOrAddressPredicates(criteriaBuilder, term));
+            }
+            return criteriaBuilder.and(predicates.toArray(new Predicate[]{}));
+        };
+    }
 
 
-			// is active record
-			if (!isEmpty(request.getIsActiveStatus())) {
-				predicates.addAll(activeRecordPredicate(criteriaBuilder, request.getIsActiveStatus(), mainRoot));
-			}
+    public static Specification<CompanyCustomer> companyCustomerSpecification(CompanyCustomerSearchRequest request) {
+        return (root, criteriaQuery, criteriaBuilder) -> {
 
-			// registered by
-			if (!isEmpty(request) && !isEmpty(request.getRegisteredBy())) {
-				predicates.add(registeredByPredicate(criteriaBuilder, request.getRegisteredBy()));
-			}
+            mainRoot = root;
+            List<Predicate> predicates = new ArrayList<>();
+
+            // customer full name
+            if (!isEmpty(request) && !isEmpty(request.getCustomerFullName())) {
+                predicates.addAll(fullNamePredicates(criteriaBuilder, request.getCustomerFullName()));
+            }
+
+            // customer phone
+            if (!isEmpty(request) && !isEmpty(request.getCustomerPhoneNumber())) {
+                predicates.add(phoneNumberPredicate(criteriaBuilder, request.getCustomerPhoneNumber()));
+            }
+
+            // customer email
+            if (!isEmpty(request) && !isEmpty(request.getCustomerEmail())) {
+                predicates.add(emailPredicate(criteriaBuilder, request.getCustomerEmail()));
+            }
+
+            // date range
+            if (!isEmpty(request.getDateRangeParam())) {
+                predicates.addAll(dateRangeParamPredicates(criteriaBuilder, request.getDateRangeParam(), mainRoot));
+            }
+
+            // customer tag
+            if (!isEmpty(request) && !isEmpty(request.getCustomerTag())) {
+                predicates.add(customerTagPredicate(criteriaBuilder, request.getCustomerTag()));
+            }
+
+
+            // is active record
+            if (!isEmpty(request.getIsActiveStatus())) {
+                predicates.addAll(activeRecordPredicate(criteriaBuilder, request.getIsActiveStatus(), mainRoot));
+            }
+
+            // registered by
+            if (!isEmpty(request) && !isEmpty(request.getRegisteredBy())) {
+                predicates.add(registeredByPredicate(criteriaBuilder, request.getRegisteredBy()));
+            }
 
 
 			/* todo:: add searchPredicates for the following
@@ -76,40 +89,59 @@ public class CompanyCustomerSearchSpecification {
 				ProductRequest productRequest -> search all customers who have purchased this product
 				String promotion -> search all customers who have used this promotion
 			*/
-			return criteriaBuilder.and(predicates.toArray(new Predicate[] { }));
-		};
-	}
+            return criteriaBuilder.and(predicates.toArray(new Predicate[]{}));
+        };
+    }
 
-	private static List<Predicate> fullNamePredicates(CriteriaBuilder criteriaBuilder, String fullName) {
-		List<Predicate> predicates = new ArrayList<>();
-		Predicate firstNamePredicate = customerFirstNamePredicate(criteriaBuilder, fullName);
-		Predicate lastNamePredicate = lastNamePredicate(criteriaBuilder, fullName);
-		predicates.add(criteriaBuilder.or(firstNamePredicate, lastNamePredicate));
-		return predicates;
-	}
+    private static List<Predicate> nameEmailPhoneOrAddressPredicates(CriteriaBuilder criteriaBuilder, String term) {
+        List<Predicate> predicates = new ArrayList<>();
 
-	private static Predicate customerFirstNamePredicate(CriteriaBuilder criteriaBuilder, String name) {
-		return criteriaBuilder.like(criteriaBuilder.lower(mainRoot.get("customerFirstName")), "%" + name + "%");
-	}
+        Predicate firstNamePredicate = customerFirstNamePredicate(criteriaBuilder, term);
+        Predicate lastNamePredicate = lastNamePredicate(criteriaBuilder, term);
+        Predicate emailPredicate = emailPredicate(criteriaBuilder, term);
+        Predicate phonePredicate = phoneNumberPredicate(criteriaBuilder, term);
+        Predicate addressPredicate = addressPredicate(criteriaBuilder, term);
 
-	private static Predicate lastNamePredicate(CriteriaBuilder criteriaBuilder, String name) {
-		return criteriaBuilder.like(criteriaBuilder.lower(mainRoot.get("customerLastName")), "%" + name + "%");
-	}
+        predicates.add(criteriaBuilder.or(
+                firstNamePredicate, lastNamePredicate, emailPredicate, phonePredicate, addressPredicate
+        ));
+        return predicates;
+    }
 
-	private static Predicate phoneNumberPredicate(CriteriaBuilder criteriaBuilder, String search) {
-		return criteriaBuilder.like(criteriaBuilder.lower(mainRoot.get("customerPhone")), "%" + search + "%");
-	}
+    private static List<Predicate> fullNamePredicates(CriteriaBuilder criteriaBuilder, String fullName) {
+        List<Predicate> predicates = new ArrayList<>();
+        Predicate firstNamePredicate = customerFirstNamePredicate(criteriaBuilder, fullName);
+        Predicate lastNamePredicate = lastNamePredicate(criteriaBuilder, fullName);
+        predicates.add(criteriaBuilder.or(firstNamePredicate, lastNamePredicate));
+        return predicates;
+    }
 
-	private static Predicate emailPredicate(CriteriaBuilder criteriaBuilder, String search) {
-		return criteriaBuilder.like(criteriaBuilder.lower(mainRoot.get("customerEmail")), "%" + search + "%");
-	}
+    private static Predicate customerFirstNamePredicate(CriteriaBuilder criteriaBuilder, String name) {
+        return criteriaBuilder.like(criteriaBuilder.lower(mainRoot.get("customerFirstName")), "%" + name + "%");
+    }
 
-	private static Predicate customerTagPredicate(CriteriaBuilder criteriaBuilder, CustomerTagEnum tagEnum) {
-		return criteriaBuilder.equal(mainRoot.get("customerTag"), tagEnum);
-	}
+    private static Predicate lastNamePredicate(CriteriaBuilder criteriaBuilder, String name) {
+        return criteriaBuilder.like(criteriaBuilder.lower(mainRoot.get("customerLastName")), "%" + name + "%");
+    }
 
-	private static Predicate registeredByPredicate(CriteriaBuilder criteriaBuilder, String username) {
-		return criteriaBuilder.equal(mainRoot.get("registeredBy"), username);
-	}
+    private static Predicate phoneNumberPredicate(CriteriaBuilder criteriaBuilder, String search) {
+        return criteriaBuilder.like(criteriaBuilder.lower(mainRoot.get("customerPhone")), "%" + search + "%");
+    }
+
+    private static Predicate emailPredicate(CriteriaBuilder criteriaBuilder, String search) {
+        return criteriaBuilder.like(criteriaBuilder.lower(mainRoot.get("customerEmail")), "%" + search + "%");
+    }
+
+    private static Predicate addressPredicate(CriteriaBuilder criteriaBuilder, String search) {
+        return criteriaBuilder.like(criteriaBuilder.lower(mainRoot.get("customerAddress")), "%" + search + "%");
+    }
+
+    private static Predicate customerTagPredicate(CriteriaBuilder criteriaBuilder, CustomerTagEnum tagEnum) {
+        return criteriaBuilder.equal(mainRoot.get("customerTag"), tagEnum);
+    }
+
+    private static Predicate registeredByPredicate(CriteriaBuilder criteriaBuilder, String username) {
+        return criteriaBuilder.equal(mainRoot.get("registeredBy"), username);
+    }
 
 }
