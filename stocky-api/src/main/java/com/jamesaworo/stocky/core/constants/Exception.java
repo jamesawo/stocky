87680416240c625926e3be2c0e0567e3fc5ce530
James Aworo <james.aworo@outlook.com>
2023-07-19 23:54:43 +0100

    update(exception): add SqlError exception handler

--- stocky-api/src/main/java/com/jamesaworo/stocky/core/constants/Exception.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/core/constants/Exception.java
@@ -7,4 +7,5 @@ package com.jamesaworo.stocky.core.constants;
 public class Exception {
     public static final String RECORD_NOT_FOUND = "The record could not be found, check and try again";
     public static final String REQUIRED_ID = "The ID field is missing. please check and try again";
+    public static final String VALIDATION_ERROR = "You did not provide a proper request";
 }
