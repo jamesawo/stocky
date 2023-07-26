19cdb84d0e3d91292dd5d633a0a3705def96787b
James Aworo <james.aworo@outlook.com>
2023-07-26 12:24:54 +0100

    update(web-sales): update settings component, refactor the switch toggle

--- stocky-web/src/app/routes/settings/setting-sales/setting-sales.component.ts
+++ stocky-web/src/app/routes/settings/setting-sales/setting-sales.component.ts
@@ -1,29 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Crumbs } from '../../../shared/components/breadcrumbs/breadcrumbs.component';
-import { Observable } from 'rxjs';
-import { SettingPayload } from '../_data/setting.payload';
-import { SaleSettingUsecase } from '../_usecase/sale-setting.usecase';
+import {Component, OnInit} from '@angular/core';
+import {NzNotificationService} from 'ng-zorro-antd/notification';
+import {Observable} from 'rxjs';
+import {Crumbs} from '../../../shared/components/breadcrumbs/breadcrumbs.component';
+import {handleUsecaseRequest} from '../../../shared/utils/util';
+import {SettingPayload} from '../_data/setting.payload';
+import {SaleSettingUsecase} from '../_usecase/sale-setting.usecase';
 
 @Component({
     selector: 'app-setting-sales',
     templateUrl: './setting-sales.component.html',
-    styles: [],
+    styles: []
 })
 export class SettingSalesComponent implements OnInit {
     public crumbs: Crumbs[] = [
-        { link: '/dashboard', title: 'Dashboard' },
-        { title: 'Setting' },
-        { link: '/settings/sales', title: 'Sales Setting ' },
+        {link: '/dashboard', title: 'Dashboard'},
+        {title: 'Setting'},
+        {link: '/settings/sales', title: 'Sales Setting '}
     ];
     public settings!: Observable<SettingPayload[]>;
 
-    constructor(private service: SaleSettingUsecase) {}
+    constructor(
+        private service: SaleSettingUsecase,
+        private notification: NzNotificationService
+    ) {}
 
     public ngOnInit(): void {
         this.settings = this.service.getSettings();
     }
 
-    public submitForm = (val: SettingPayload[]) => {
-        console.log(val);
+    public submitForm = async (settings: SettingPayload[]) => {
+        await handleUsecaseRequest(this.service.updateSettings(settings), this.notification);
     };
 }
