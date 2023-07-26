cda34099d4899259d2f53cea0baec235cb805030
James Aworo <james.aworo@outlook.com>
2023-07-26 18:29:02 +0100

    update(api-settings):refactor application setting module

--- stocky-api/src/main/java/com/jamesaworo/stocky/features/settings/data/interactor/base/SettingInteractor.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/settings/data/interactor/base/SettingInteractor.java
@@ -22,28 +22,60 @@ public class SettingInteractor implements ISettingBaseInteractor {
 
     private final ApplicationContext context;
 
-
+    /**
+     * This method is used to retrieve a specific SettingRequest object based on the given key and module.
+     * <p>
+     * It uses the SettingModule to get the ISettingBaseInteractor implementation from the ApplicationContext.
+     *
+     * @param key    the key of the SettingRequest object to be retrieved.
+     * @param module the module of the SettingRequest object.
+     * @return a ResponseEntity object containing the retrieved SettingRequest object.
+     */
     @Override
     public ResponseEntity<SettingRequest> get(String key, SettingModule module) {
         ISettingBaseInteractor interactor = module.interactor(context);
         return interactor.get(key);
     }
 
+    /**
+     * This method is used to retrieve a specific SettingRequest object based on the given key.
+     *
+     * @param key the key of the SettingRequest object to be retrieved.
+     * @return a ResponseEntity object containing the retrieved SettingRequest object.
+     */
     @Override
     public ResponseEntity<SettingRequest> get(String key) {
         return null;
     }
 
+    /**
+     * This method is used to retrieve all SettingRequest objects.
+     *
+     * @return a ResponseEntity object containing a list of all SettingRequest objects.
+     */
     @Override
     public ResponseEntity<List<SettingRequest>> getAll() {
         return null;
     }
 
+
+    /**
+     * This method is used to update a SettingRequest object.
+     *
+     * @param dto the SettingRequest object to be updated.
+     * @return a ResponseEntity object indicating the success or failure of the update operation.
+     */
     @Override
     public ResponseEntity<Boolean> update(SettingRequest dto) {
         return null;
     }
 
+    /**
+     * This method is used to update multiple SettingRequest objects.
+     *
+     * @param list the list of SettingRequest objects to be updated.
+     * @return a ResponseEntity object indicating the success or failure of the update operation.
+     */
     @Override
     public ResponseEntity<Boolean> updateAll(List<SettingRequest> list) {
         return null;
