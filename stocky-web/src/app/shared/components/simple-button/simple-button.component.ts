0d73560a03eb11c568c8b63f480c1809afae97e3
James Aworo <james.aworo@outlook.com>
2023-07-26 12:30:49 +0100

    update(web-component): add simple button component for reusability

---
+++ stocky-web/src/app/shared/components/simple-button/simple-button.component.ts
@@ -0,0 +1,53 @@
+import {Component, Input} from '@angular/core';
+import {NzButtonSize, NzButtonType} from 'ng-zorro-antd/button';
+
+@Component({
+    selector: 'app-simple-button',
+    templateUrl: './simple-button.component.html',
+    styles: []
+})
+export class SimpleButtonComponent {
+
+    @Input()
+    public usePopConfirm = false;
+
+    @Input()
+    public args?: any = {};
+
+    @Input()
+    public icon = '';
+
+    @Input()
+    public iconClass = '';
+
+    @Input()
+    public popCancelText = 'No, Cancel';
+
+    @Input()
+    public popOkText = 'Yes, Continue';
+
+    @Input()
+    public popConfirmTitle = 'Are you sure?';
+
+    @Input()
+    public popConfirmPlacement = 'top';
+
+    @Input()
+    public buttonText: string = '';
+
+    @Input()
+    public buttonType: NzButtonType = 'default';
+
+    @Input()
+    public buttonSize: NzButtonSize = 'large';
+
+    @Input()
+    public buttonClass = '';
+
+    @Input()
+    public action: (arg?: any) => void = () => {};
+
+    @Input()
+    public cancelAction = (args?: any) => {};
+
+}
