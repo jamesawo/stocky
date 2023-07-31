817b88ee5a20e7d1488a90fdac0d5a0410b4aa1e
James Aworo <james.aworo@outlook.com>
2023-07-31 17:45:28 +0100

    update(company-endpoint): update company endpoint; add is active filter to find all method

--- stocky-api/src/main/java/com/jamesaworo/stocky/features/company/endpoint/CompanyPaymentOptionEndpoint.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/company/endpoint/CompanyPaymentOptionEndpoint.java
@@ -18,42 +18,45 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.jamesaworo.stocky.core.constants.Global.API_PREFIX;
+import static java.lang.Boolean.parseBoolean;
 
 @RestController
 @RequestMapping(value = API_PREFIX + "/company/payment-option/")
 @RequiredArgsConstructor
 public class CompanyPaymentOptionEndpoint {
 
-	private final ICompanyPaymentOptionInteractor interactor;
+    private final ICompanyPaymentOptionInteractor interactor;
 
-	@GetMapping(value = "get/{id}")
-	public ResponseEntity<Optional<CommonRequest>> find(@PathVariable Long id) {
-		return this.interactor.find(id);
-	}
+    @GetMapping(value = "get/{id}")
+    public ResponseEntity<Optional<CommonRequest>> find(@PathVariable Long id) {
+        return this.interactor.find(id);
+    }
 
-	@GetMapping("all")
-	public ResponseEntity<List<CommonRequest>> findMany() {
-		return this.interactor.findAll();
-	}
+    @GetMapping("all")
+    public ResponseEntity<List<CommonRequest>> findMany(
+            @RequestParam(value = "filterIsActive") String filterIsActive
+    ) {
+        return this.interactor.findAll(parseBoolean(filterIsActive));
+    }
 
-	@PostMapping("create")
-	public ResponseEntity<Optional<CommonRequest>> save(
-			@RequestBody @Valid CommonRequest dto
-	) {
-		return this.interactor.save(dto);
-	}
+    @PostMapping("create")
+    public ResponseEntity<Optional<CommonRequest>> save(
+            @RequestBody @Valid CommonRequest dto
+    ) {
+        return this.interactor.save(dto);
+    }
 
-	@PutMapping("update")
-	public ResponseEntity<Optional<CommonRequest>> update(
-			@RequestBody @Valid CommonRequest request
-	) {
-		return this.interactor.update(request);
-	}
+    @PutMapping("update")
+    public ResponseEntity<Optional<CommonRequest>> update(
+            @RequestBody @Valid CommonRequest request
+    ) {
+        return this.interactor.update(request);
+    }
 
-	@PutMapping("status/{id}")
-	public ResponseEntity<Optional<Boolean>> toggleIsActiveStatus(@PathVariable Long id) {
-		return this.interactor.toggleActiveStatus(id);
-	}
+    @PutMapping("status/{id}")
+    public ResponseEntity<Optional<Boolean>> toggleIsActiveStatus(@PathVariable Long id) {
+        return this.interactor.toggleActiveStatus(id);
+    }
 
 
 }
