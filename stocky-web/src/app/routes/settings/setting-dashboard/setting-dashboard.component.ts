187a1637571de1c4fecbdfdb753f351c567ee850
James Aworo <james.aworo@outlook.com>
2023-08-01 22:28:31 +0100

    update(web-settings): add feature to enable settings control update after change

--- stocky-web/src/app/routes/settings/setting-dashboard/setting-dashboard.component.ts
+++ stocky-web/src/app/routes/settings/setting-dashboard/setting-dashboard.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { Crumbs } from '../../../shared/components/breadcrumbs/breadcrumbs.component';
-import { DashboardSettingUsecase } from '../_usecase/dashboard-setting.usecase';
-import { SettingPayload } from '../_data/setting.payload';
-import { Observable } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Crumbs} from '../../../shared/components/breadcrumbs/breadcrumbs.component';
+import {SettingPayload} from '../_data/setting.payload';
+import {DashboardSettingUsecase} from '../_usecase/dashboard-setting.usecase';
 
 @Component({
     selector: 'app-setting-dashboard',
-    templateUrl: './setting-dashboard.component.html',
+    templateUrl: './setting-dashboard.component.html'
 })
 export class SettingDashboardComponent implements OnInit {
     public crumbs: Crumbs[] = [
-        { link: '/dashboard', title: 'Dashboard' },
-        { title: 'Setting' },
-        { link: '/settings/dashboard', title: 'Dashboard Setting ' },
+        {link: '/dashboard', title: 'Dashboard'},
+        {title: 'Setting'},
+        {link: '/settings/dashboard', title: 'Dashboard Setting '}
     ];
     public settings!: Observable<SettingPayload[]>;
 
@@ -22,7 +22,4 @@ export class SettingDashboardComponent implements OnInit {
         this.settings = this.dashboardService.getSettings();
     }
 
-    public submitForm = (val: SettingPayload[]) => {
-        console.log(val);
-    };
 }
