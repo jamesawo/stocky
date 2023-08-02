c6548714ae297a3d8ba13626c4e90d7ffd6a5b10
James Aworo <james.aworo@outlook.com>
2023-08-02 17:30:44 +0100

    refactor(web-layout): remove unused header items, fix nav on mobile

--- stocky-web/src/app/layout/basic/widgets/user.component.ts
+++ stocky-web/src/app/layout/basic/widgets/user.component.ts
@@ -1,33 +1,36 @@
-import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
-import { SettingsService, User } from '@delon/theme';
+import {ChangeDetectionStrategy, Component, Inject} from '@angular/core';
+import {Router} from '@angular/router';
+import {DA_SERVICE_TOKEN, ITokenService} from '@delon/auth';
+import {SettingsService, User} from '@delon/theme';
 
 @Component({
     selector: 'header-user',
     template: `
         <div
+            nzTrigger="click"
             class="alain-default__nav-item d-flex align-items-center px-sm"
             nz-dropdown
-            nzPlacement="bottomRight"
             [nzDropdownMenu]="userMenu"
         >
             <nz-avatar [nzSrc]="user.avatar" nzSize="small" class="mr-sm"></nz-avatar>
             {{ user.name }}
         </div>
+
         <nz-dropdown-menu #userMenu="nzDropdownMenu">
             <div nz-menu class="width-sm">
-                <div nz-menu-item routerLink="/pro/account/center">
+                <div nz-menu-item routerLink="#">
                     <i nz-icon nzType="user" class="mr-sm"></i>
                     {{ 'Help Center' }}
+                    <!--                    <app-coming-soon-text></app-coming-soon-text>-->
                 </div>
-                <div nz-menu-item routerLink="/pro/account/settings">
+                <div nz-menu-item routerLink="#">
                     <i nz-icon nzType="setting" class="mr-sm"></i>
                     {{ 'Settings' | i18n }}
                 </div>
-                <div nz-menu-item routerLink="/exception/trigger">
-                    <i nz-icon nzType="close-circle" class="mr-sm"></i>
-                    {{ 'Trigger' | i18n }}
+                <li nz-menu-divider></li>
+                <div nz-menu-item routerLink="#">
+                    <i nz-icon nzType="credit-card" class="mr-sm"></i>
+                    {{ 'Shit Report' | i18n }}
                 </div>
                 <li nz-menu-divider></li>
                 <div nz-menu-item (click)="logout()">
@@ -40,11 +43,11 @@ import { SettingsService, User } from '@delon/theme';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderUserComponent {
+    constructor(private settings: SettingsService, private router: Router, @Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService) {}
+
     get user(): User {
         return this.settings.user;
     }
-
-    constructor(private settings: SettingsService, private router: Router, @Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService) {}
 
     logout(): void {
         this.tokenService.clear();
--- stocky-web/src/app/layout/passport/passport.component.ts
+++ stocky-web/src/app/layout/passport/passport.component.ts
@@ -1,30 +1,30 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
+import {Component, Inject, OnInit} from '@angular/core';
+import {DA_SERVICE_TOKEN, ITokenService} from '@delon/auth';
 
 @Component({
-  selector: 'layout-passport',
-  templateUrl: './passport.component.html',
-  styleUrls: ['./passport.component.less']
+    selector: 'layout-passport',
+    templateUrl: './passport.component.html',
+    styleUrls: ['./passport.component.less']
 })
 export class LayoutPassportComponent implements OnInit {
-  links = [
-    {
-      title: 'Support',
-      href: ''
-    },
-    {
-      title: 'Developers',
-      href: ''
-    },
-    {
-      title: 'FAQ',
-      href: ''
+    links = [
+        {
+            title: 'Support',
+            href: ''
+        },
+        {
+            title: 'Documentation',
+            href: ''
+        },
+        {
+            title: 'FAQ',
+            href: ''
+        }
+    ];
+
+    constructor(@Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService) {}
+
+    ngOnInit(): void {
+        this.tokenService.clear();
     }
-  ];
-
-  constructor(@Inject(DA_SERVICE_TOKEN) private tokenService: ITokenService) {}
-
-  ngOnInit(): void {
-    this.tokenService.clear();
-  }
 }
