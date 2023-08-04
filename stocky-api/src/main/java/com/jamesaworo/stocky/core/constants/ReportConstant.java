ed46559bf866fd36edf3b408b8353423bb07ed24
James Aworo <james.aworo@outlook.com>
2023-08-04 22:43:26 +0100

    update(api-core): update report constants

--- stocky-api/src/main/java/com/jamesaworo/stocky/core/constants/ReportConstant.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/core/constants/ReportConstant.java
@@ -11,6 +11,10 @@ public class ReportConstant {
     public static final String EMPTY = "";
     public static final String NOT_AVAILABLE = "N/A";
     public static final String RECEIPT_DISCLAIMER = "GOODS BOUGHT IN GOOD CONDITION ARE NONRETURNABLE. THANK YOU FOR YOUR PATRONAGE";
-    public static final String RECEIPT_FOOTER = "Powered by @Stock www.stocky-pos.com";
+    public static final String RECEIPT_FOOTER = "Powered by @Stocky | www.stocky-pos.com";
     public static final String SALES_REPORT_TITLE = "DAILY SALES COLLECTION REPORT";
+    public static final String RECEIPT_NOT_FOUND = "Receipt not found, Invalid details supplied";
+    public static final String REPORT_FILE_NAME = "inline; filename=\"Report.pdf\"";
+    public static final String RECEIPT_FILE_NAME = "inline; filename=\"Receipt.pdf\"";
+    public static final String PDF_CONTENT_TYPE = "application/pdf; charset=UTF-8";
 }
