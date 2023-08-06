a32992c4bbcb175b5b9c00fc37f1b418ad964a02
James Aworo <james.aworo@outlook.com>
2023-08-06 21:47:33 +0100

    update(api-config): update modelmapper configuration class

--- stocky-api/src/main/java/com/jamesaworo/stocky/configuration/configurer/ModelMapperConfig.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/configuration/configurer/ModelMapperConfig.java
@@ -12,33 +12,33 @@ import java.time.LocalDate;
  * @since 5/12/23
  */
 @Component
-public class Config {
+public class ModelMapperConfig {
 
-	Provider<LocalDate> localDateProvider = new AbstractProvider<LocalDate>() {
-		@Override
-		public LocalDate get() {
-			return LocalDate.now();
-		}
-	};
+    Provider<LocalDate> localDateProvider = new AbstractProvider<LocalDate>() {
+        @Override
+        public LocalDate get() {
+            return LocalDate.now();
+        }
+    };
 
-	Converter<String, LocalDate> toStringDate = new AbstractConverter<String, LocalDate>() {
-		@Override
-		protected LocalDate convert(String source) {
-			return new LocalDateStringConverter().convert(source);
-		}
-	};
+    Converter<String, LocalDate> toStringDate = new AbstractConverter<String, LocalDate>() {
+        @Override
+        protected LocalDate convert(String source) {
+            return new LocalDateStringConverter().convert(source);
+        }
+    };
 
 
-	@Bean
-	public ModelMapper modelMapper() {
-		ModelMapper mapper = new ModelMapper();
-		mapper.getConfiguration().setSkipNullEnabled(true);
-		mapper.getConfiguration().setAmbiguityIgnored(true);
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setSkipNullEnabled(true);
+        mapper.getConfiguration().setAmbiguityIgnored(true);
 
-		mapper.createTypeMap(String.class, LocalDate.class);
-		mapper.addConverter(toStringDate);
-		mapper.getTypeMap(String.class, LocalDate.class).setProvider(localDateProvider);
-		
-		return mapper;
-	}
+        mapper.createTypeMap(String.class, LocalDate.class);
+        mapper.addConverter(toStringDate);
+        mapper.getTypeMap(String.class, LocalDate.class).setProvider(localDateProvider);
+
+        return mapper;
+    }
 }
