9c694de9e36db5951803c7c9c543439fd10e627c
James Aworo <james.aworo@outlook.com>
2023-08-07 17:20:01 +0100

    update(api-core): update core module; add constants values for route params

--- stocky-api/src/main/java/com/jamesaworo/stocky/core/commandrunner/Runner.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/core/commandrunner/Runner.java
@@ -3,6 +3,7 @@ package com.jamesaworo.stocky.core.commandrunner;
 import com.jamesaworo.stocky.core.commandrunner.seeders.CompanySeeder;
 import com.jamesaworo.stocky.core.commandrunner.seeders.PermissionSeeder;
 import com.jamesaworo.stocky.core.commandrunner.seeders.SettingSeeder;
+import com.jamesaworo.stocky.core.commandrunner.seeders.UserSeeder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,25 +15,28 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Runner implements CommandLineRunner {
-	private final SettingSeeder settingSeeder;
-	private final CompanySeeder companySeeder;
-	private final PermissionSeeder permissionSeeder;
+    private final SettingSeeder settingSeeder;
+    private final CompanySeeder companySeeder;
+    private final PermissionSeeder permissionSeeder;
+    private final UserSeeder userSeeder;
 
-	@Autowired
-	public Runner(
-			SettingSeeder settingSeeder,
-			CompanySeeder companySeeder,
-			PermissionSeeder permissionSeeder
-	) {
-		this.settingSeeder = settingSeeder;
-		this.companySeeder = companySeeder;
-		this.permissionSeeder = permissionSeeder;
-	}
+    @Autowired
+    public Runner(
+            SettingSeeder settingSeeder,
+            CompanySeeder companySeeder,
+            PermissionSeeder permissionSeeder,
+            UserSeeder userSeeder) {
+        this.settingSeeder = settingSeeder;
+        this.companySeeder = companySeeder;
+        this.permissionSeeder = permissionSeeder;
+        this.userSeeder = userSeeder;
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		settingSeeder.run();
-		companySeeder.run();
-		permissionSeeder.run();
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        settingSeeder.run();
+        companySeeder.run();
+        permissionSeeder.run();
+        userSeeder.run();
+    }
 }
--- stocky-api/src/main/java/com/jamesaworo/stocky/core/commandrunner/seeders/PermissionSeeder.java
+++ stocky-api/src/main/java/com/jamesaworo/stocky/core/commandrunner/seeders/PermissionSeeder.java
@@ -8,63 +8,85 @@
 package com.jamesaworo.stocky.core.commandrunner.seeders;
 
 import com.jamesaworo.stocky.features.authentication.data.repository.PermissionRepository;
+import com.jamesaworo.stocky.features.authentication.data.repository.RoleRepository;
+import com.jamesaworo.stocky.features.authentication.domain.entity.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+
 import static com.jamesaworo.stocky.core.commandrunner.seeds.PermissionSeeds.*;
+import static com.jamesaworo.stocky.core.constants.Setting.DEFAULT_SYS_ROLE;
 
 @Component
 @RequiredArgsConstructor
 public class PermissionSeeder {
-	private final PermissionRepository permissionRepository;
+    private final PermissionRepository permissionRepository;
+    private final RoleRepository roleRepository;
 
-	public void run() {
-		if (this.permissionRepository.count() == 0) {
-			runCompanyPermissionSeeder();
-			runPaywallPermissionSeeder();
-			runProductPermissionSeeder();
-			runReportPermissionSeeder();
-			runSalePermissionSeeder();
-			runSettingsPermissionSeeder();
-			runStockPermissionSeeder();
-		}
+    public void run() {
+        if (this.permissionRepository.count() == 0) {
+            runCompanyAuthenticationSeeder();
+            runCompanyPermissionSeeder();
+            runPaywallPermissionSeeder();
+            runProductPermissionSeeder();
+            runReportPermissionSeeder();
+            runSalePermissionSeeder();
+            runSettingsPermissionSeeder();
+            runStockPermissionSeeder();
+            runSeedRole();
+        }
 
-	}
+    }
 
-	private void runCompanyPermissionSeeder() {
-		this.permissionRepository.saveAll(COMPANY_PERMISSIONS);
-		System.out.println("----- seed company permission  -----");
-	}
+    private void runCompanyAuthenticationSeeder() {
+        this.permissionRepository.saveAll(AUTH_PERMISSIONS);
+        System.out.println("----- seed authentication permission  -----");
+    }
 
-	private void runPaywallPermissionSeeder() {
-		this.permissionRepository.saveAll(PAYWALL_PERMISSIONS);
-		System.out.println("----- seed paywall permission  -----");
-	}
+    private void runCompanyPermissionSeeder() {
+        this.permissionRepository.saveAll(COMPANY_PERMISSIONS);
+        System.out.println("----- seed company permission  -----");
+    }
 
-	private void runProductPermissionSeeder() {
-		this.permissionRepository.saveAll(PRODUCT_PERMISSIONS);
-		System.out.println("----- seed product permission  -----");
-	}
+    private void runPaywallPermissionSeeder() {
+        this.permissionRepository.saveAll(PAYWALL_PERMISSIONS);
+        System.out.println("----- seed paywall permission  -----");
+    }
 
-	private void runReportPermissionSeeder() {
-		this.permissionRepository.saveAll(REPORT_PERMISSIONS);
-		System.out.println("----- seed report permission  -----");
-	}
+    private void runProductPermissionSeeder() {
+        this.permissionRepository.saveAll(PRODUCT_PERMISSIONS);
+        System.out.println("----- seed product permission  -----");
+    }
 
-	private void runSalePermissionSeeder() {
-		this.permissionRepository.saveAll(SALE_PERMISSIONS);
-		System.out.println("----- seed sale permission  -----");
-	}
+    private void runReportPermissionSeeder() {
+        this.permissionRepository.saveAll(REPORT_PERMISSIONS);
+        System.out.println("----- seed report permission  -----");
+    }
 
-	private void runSettingsPermissionSeeder() {
-		this.permissionRepository.saveAll(SETTING_PERMISSIONS);
-		System.out.println("----- seed settings permission  -----");
-	}
+    private void runSalePermissionSeeder() {
+        this.permissionRepository.saveAll(SALE_PERMISSIONS);
+        System.out.println("----- seed sale permission  -----");
+    }
 
-	private void runStockPermissionSeeder() {
-		this.permissionRepository.saveAll(STOCK_PERMISSIONS);
-		System.out.println("----- seed stock permission  -----");
-	}
+    private void runSettingsPermissionSeeder() {
+        this.permissionRepository.saveAll(SETTING_PERMISSIONS);
+        System.out.println("----- seed settings permission  -----");
+    }
 
+    private void runStockPermissionSeeder() {
+        this.permissionRepository.saveAll(STOCK_PERMISSIONS);
+        System.out.println("----- seed stock permission  -----");
+    }
+
+    private void runSeedRole() {
+        if (this.roleRepository.count() == 0 || this.roleRepository.findByNameEqualsIgnoreCase(DEFAULT_SYS_ROLE).isEmpty()) {
+            Role role = Role.builder().name(DEFAULT_SYS_ROLE)
+                    .description("Default system role")
+                    .permissions(new HashSet<>(this.permissionRepository.findAll())).build();
+            this.roleRepository.save(role);
+            System.out.println("----- seed role  -----");
+        }
+    }
 
 }
