0142801ba81483d6a49816bdb3c5e415ad341927
James Aworo <james.aworo@outlook.com>
2023-08-09 02:31:33 +0100

    update(shared-component): update button component; add disable props

---
+++ stocky-web/src/app/shared/components/setting-locker/setting-locker.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+    selector: 'app-setting-locker',
+    templateUrl: './setting-locker.component.html',
+    styles: []
+})
+export class SettingLockerComponent {
+
+    @Input()
+    public disabled: boolean = false;
+
+    @Input()
+    public toolTip: string = 'This feature is currently disabled in settings';
+
+}
