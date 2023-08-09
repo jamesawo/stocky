9be776996b7b09bc8eab61a0da5fa1ffd5cf59cc
James Aworo <james.aworo@outlook.com>
2023-08-09 19:41:34 +0100

    update(service): add file service

---
+++ stocky-web/src/app/shared/utils/upload-import.service.ts
@@ -0,0 +1,39 @@
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {NzDrawerService} from 'ng-zorro-antd/drawer';
+import {UploadComponentInput} from '../../data/payload/common.interface';
+import {UploadFileComponent} from '../components/upload-file/upload-file.component';
+import {ResponsiveService} from './responsive.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UploadImportService {
+
+    constructor(
+        private drawerService: NzDrawerService,
+        private http: HttpClient,
+        private responsiveService: ResponsiveService
+    ) { }
+
+    public upload(arg: UploadComponentInput, title?: string) {
+        return this.drawerService
+            .create<UploadFileComponent, UploadComponentInput, void>({
+                nzTitle: title ?? 'Upload Files',
+                nzFooter: ' ',
+                nzExtra: ' ',
+                nzClosable: true,
+                nzMaskClosable: false,
+                nzWidth: this.getDrawerSize(),
+                nzContent: UploadFileComponent,
+                nzContentParams: {...arg}
+            });
+    }
+
+    public download() {}
+
+    private getDrawerSize(): number {
+        const value = this.responsiveService.screenWidth$.value;
+        return value > 700 ? 720 : 350;
+    }
+}
