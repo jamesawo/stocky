c0235adabf2ea6bc5cb5fef1db0fd77ff4a6b4e2
James Aworo <james.aworo@outlook.com>
2023-08-24 22:51:38 +0100

    update(authentication): add manage account api route

---
+++ stocky-api/src/main/java/com/jamesaworo/stocky/features/authentication/data/request/AccountRequest.java
@@ -0,0 +1,32 @@
+/*
+ * @Author: james.junior
+ * @Date: 8/24/23 18:19
+ *
+ * @Project: stocky-api
+ */
+
+package com.jamesaworo.stocky.features.authentication.data.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AccountRequest {
+    private Long employeeId;
+    private Long userId;
+    private String name;
+    private String phone;
+    private String username;
+    private String password;
+    private Collection<RoleRequest> roles;
+    private Boolean isActiveStatus;
+}
